913aff1d3239c5b169fc46c9f114a332e45e52f4
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2013-02-28 13:03:12 +0100

    Prova pullme automatico 4

--- pullme.php
+++ pullme.php
@@ -1,11 +1,9 @@
 <?php
  
+$GIT_BIN = '/usr/bin/git';
+$REMOTE = 'origin';
+$BRANCH = 'master';
+
 header('Content-Type: text/plain');
- 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    die('Richiesta non valida');
-}
 
-echo "Risultato:\n\n";
-
-system("git pull");
+system("$GIT_BIN fetch $REMOTE 2>&1; $GIT_BIN checkout -q $REMOTE/$BRANCH 2>&1; $GIT_BIN log -1 2>&1;");
