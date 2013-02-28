c9d24e88d93a98b92e42e1597466771b77d12a58
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2013-02-28 12:27:42 +0100

    Pullme per l'hook automatico su GitHub (al push, beta/auto pullerà in automatico)

---
+++ pullme.php
@@ -0,0 +1,13 @@
+<?php
+ 
+$GIT_BIN = '/usr/bin/git';
+ $REMOTE = 'origin';
+$BRANCH = 'master';
+
+header('Content-Type: text/plain');
+ 
+if (!isset($argv) && $_SERVER['REQUEST_METHOD'] !== 'POST') {
+    die('Richiesta non valida');
+}
+
+system("$GIT_BIN fetch $REMOTE 2>&1; $GIT_BIN checkout -q $REMOTE/$BRANCH 2>&1; $GIT_BIN log -1 2>&1;");
