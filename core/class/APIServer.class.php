92b1a8603a5eeb77b36ff58f988fe1d63583313a
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2013-02-28 21:24:28 +0100

    Bug risolto: in competenze personali la ricerca ritornava tutti i tipi di titolo

--- core/class/APIServer.class.php
+++ core/class/APIServer.class.php
@@ -117,7 +117,7 @@ class APIServer {
         
         public function api_cercaTitolo() {
             $t = [];
-            if (!$this->par['t']) { $this->par['t'] = -1; }
+            if (!isset($this->par['t'])) { $this->par['t'] = -1; }
             foreach ( Titolo::cerca($this->par['query'], $this->par['t']) as $titolo ) {
                 $t[] = [$titolo->id, $titolo->nome];
             }
