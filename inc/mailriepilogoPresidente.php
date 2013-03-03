f53802a4c68a661282ce667543fb2e02db854d71
Federico D'Urso <ico88@hotmail.com>
2013-03-03 01:35:38 +0100

    Riepilogo appartenenze pendenti per presidente

--- inc/mailriepilogoPresidente.php
+++ inc/mailriepilogoPresidente.php
@@ -4,14 +4,14 @@
  * ©2012 Croce Rossa Italiana
  */
 
-foreach ( Comitato::lista() as $comitato ) {
+foreach ( Comitato::elenco() as $comitato ) {
   $presidenti = $comitato->membriAttuali(MEMBRO_PRESIDENTE);
-  $pendenti = $comitato->membriPendenti();
+  $pendenti = $comitato->appartenenzePendenti();
   $numPendenti = count($pendenti);
   foreach ( $presidenti as $presidente ) {
     $m = new Email('riepilogoPresidente','Riepilogo giornaliero pendenze su Gaia');
     $m->a = $presidente;
-    $m->_apppendenti = $pendenti;
+    $m->_APPPENDENTI = $numPendenti;
     $m->invia();
    }
 }
