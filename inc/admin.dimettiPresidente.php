babfaf2881d171a6a84c329ffb0ae31e046d000d
Federico D'Urso <ico88@hotmail.com>
2013-03-04 21:13:53 +0100

    Errore con il dimetti presidente, creava nuova appartenenza

--- inc/admin.dimettiPresidente.php
+++ inc/admin.dimettiPresidente.php
@@ -7,14 +7,14 @@
 $t = $_GET['id'];
 $f = new Appartenenza($t);
 $f->fine = time();
-$a = new Appartenenza();
+/*$a = new Appartenenza();
 $a->volontario  = $f->volontario()->id;
 $a->comitato    = $f->comitato()->id;
 $a->inizio      = time();
 $a->fine        = strtotime('April 31');
 $a->stato =     MEMBRO_VOLONTARIO;
 $a->conferma =  $me->id;
-$a->timestamp = time();
+$a->timestamp = time();*/
 
 redirect('admin.Presidenti&ok');
 
