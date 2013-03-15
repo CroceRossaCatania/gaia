ccee2ec5848afe72cb8ad9f49c1f89f574a622a7
Federico D'Urso <ico88@hotmail.com>
2013-03-15 18:23:35 +0100

    Creata la pagina di inserimento attività

--- inc/attivita.nuova.php
+++ inc/attivita.nuova.php
@@ -3,19 +3,19 @@ $a = new Attivita(@$_GET['id']);
 ?>
 
 <div class="row-fluid">
-    
-<div class="span4">
-    <h2>Attività</h2>
-    <p>Modulo creazione o modifica attività.</p>
-    <div class="alert alert-warning alert-block">
+    <div class="span7">    
+        <div class="row-fluid">
+            <h2>
+                <i class="icon-list-alt"></i>
+                Attività
+            </h2>
+        </div>
+    <div class="alert alert-error alert-block">
         <h4><i class="icon-warning-sign"></i> Altamente sperimentale</h4>
         Potrebbe uccidere il tuo gatto.</p>
     </div>
-</div>
 
-<div class="span8">
-          <form class="form-horizontal" action="?p=attivita.nuova.ok" method="POST">
-          <input type="hidden" name="id" value="<?php echo $a->id; ?>" />
+          <div class="form-horizontal">
           <div class="control-group">
             <label class="control-label" for="inputNome">Nome</label>
             <div class="controls">
@@ -23,9 +23,9 @@ $a = new Attivita(@$_GET['id']);
             </div>
           </div>
           <div class="control-group">
-            <label class="control-label" for="inputComitato">Comitato</label>
+            <label class="control-label" for="inputRivolto">A chi è rivolta l'attività</label>
             <div class="controls">
-                <select required name="inputComitato" id="inputComitato" autofocus class="span11">
+                <select required name="inputRivolto" id="inputRivolto" autofocus class="input-xlarge">
                     <option value="" selected="selected">[ Seleziona un Comitato ]</option>
                     <?php foreach ( $me->comitati() as $c ) { ?>
                         <option value="<?php echo $c->id; ?>"><?php echo $c->nome; ?></option>
@@ -34,79 +34,49 @@ $a = new Attivita(@$_GET['id']);
             </div>
           </div>
           <div class="control-group">
-            <label class="control-label" for="inputCodiceFiscale">Cod. Fiscale</label>
+            <label class="control-label" for="inputTipo">Tipo attività</label>
             <div class="controls">
-              <input value="<?php echo $p->codiceFiscale; ?>" class="input-large" readonly="readonly" type="text" id="inputCodiceFiscale" name="inputCodiceFiscale" required  pattern="[A-Za-z0-9]{16}" />
-            </div>
-          </div>
-          <div class="control-group">
-            <label class="control-label">Data di nascita</label>
-            <?php $dataNascita = date('d-m-Y', $p->dataNascita); $array=explode('-',$dataNascita);?>
-            <div class="controls ">
-                <input type="number" required min="1" max="31" class="input-mini" value="<?php if($array[0]!=''){ echo $array[0];}else{ ?> 1 <?php }?>" name="inputGiorno"/>
-                <select class="input-medium" name="inputMese">
-                    <?php for ( $i = 1; $i <= 12; $i++ ) { ?>
-                        <option value="<?php echo $i ?>" <?php if ( $i == $array[1] ) { ?>selected<?php } ?>><?php echo $conf['mesi'][$i]; ?></option>
+                <select required name="inputTipo" id="inputTipo" autofocus class="input-xlarge">
+                    <option value="" selected="selected">[ Seleziona una Categoria ]</option>
+                    <?php foreach ( $me->comitati() as $c ) { ?>
+                        <option value="<?php echo $c->id; ?>"><?php echo $c->nome; ?></option>
                     <?php } ?>
                 </select>
-              <input type="number" required min="1900" max="2006" require value="<?php if($array[2]!=''){ echo $array[2];}else{ ?> 1994 <?php }?>" class="input-mini" name="inputAnno"/>
             </div>
           </div>
           <div class="control-group">
-            <label class="control-label" for="inputProvinciaNascita">Provincia di Nascita</label>
+            <label class="control-label" for="inputDescrizione">Descrizione attività</label>
             <div class="controls">
-              <input value="<?php echo $p->ProvinciaNascita; ?>" class="input-mini" type="text" id="inputProvinciaNascita" name="inputProvinciaNascita" required pattern="[A-Za-z]{2}" />
-              &nbsp; <span class="muted">ad es.: CT</span>
+              <textarea rows="7" class="input-xlarge" type="text" id="inputDescrizione" name="inputDesrizione" placeholder="Es.: Attività rivolta agli indigenti" required pattern=".{2,}"></textarea>
             </div>
           </div>
-          <div class="control-group">
-            <label class="control-label" for="inputComuneNascita">Comune di Nascita</label>
-            <div class="controls">
-              <input value="<?php echo $p->ComuneNascita; ?>" type="text" id="inputComuneNascita" name="inputComuneNascita" required />
-            </div>
+          
+       <div class="span12">
+       <table class="table table-striped table-bordered table-condensed" id="tabellaTurni">
+            <thead>
+                <th>Turno</th>
+                <th>Inizio</th>
+                <th>Fine</th>
+            </thead>
+            <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            <tr>
+                <td colspan="3"><button id="aggiungiTurno" class="btn btn-block btn-primary">
+                  <i class="icon-plus"></i>
+                  Aggiungi turno
+              </button></td>
+                
+                
+            </tr>
+        </table>
+          
           </div>
-          <div class="control-group">
-            <label class="control-label" for="inputIndirizzo">Indirizzo</label>
-            <div class="controls">
-              <input value="<?php echo $p->indirizzo; ?>" type="text" id="inputIndirizzo" name="inputIndirizzo" required />
-            </div>
-          </div>
-          <div class="control-group">
-            <label class="control-label" for="inputCivico">Civico</label>
-            <div class="controls">
-              <input value="<?php echo $p->civico; ?>" type="text" id="inputCivico" name="inputCivico" class="input-small" required />
-            </div>
-          </div>
-          <div class="control-group">
-            <label class="control-label" for="inputComuneResidenza">Comune di residenza</label>
-            <div class="controls">
-              <input value="<?php echo $p->comuneResidenza; ?>" type="text" id="inputComuneResidenza" name="inputComuneResidenza" required />
-            </div>
-          </div>
-          <div class="control-group">
-            <label class="control-label" for="inputCAPResidenza">CAP di residenza</label>
-            <div class="controls">
-              <input value="<?php echo $p->CAPResidenza; ?>" class="input-small" type="text" id="inputCAPResidenza" name="inputCAPResidenza" required pattern="[0-9]{5}" />
-            </div>
-          </div>
-          <div class="control-group">
-            <label class="control-label" for="inputProvinciaResidenza">Provincia di residenza</label>
-            <div class="controls">
-              <input value="<?php echo $p->provinciaResidenza; ?>" class="input-mini" type="text" id="inputProvinciaResidenza" name="inputProvinciaResidenza" required pattern="[A-Za-z]{2}" />
-              &nbsp; <span class="muted">ad es.: CT</span>
-            </div>
-          </div>
-          <div class="control-group">
-            <label class="control-label" for="inputgruppoSanguigno">Gruppo Sanguigno</label>
-            <div class="controls">
-                <select class="input-small" id="inputgruppoSanguigno" name="inputgruppoSanguigno"  required>
-                <?php
-                    foreach ( $conf['sangue_gruppo'] as $numero => $gruppo ) { ?>
-                    <option value="<?php echo $numero; ?>" <?php if ( $numero == $me->grsanguigno ) { ?>selected<?php } ?>><?php echo $gruppo; ?></option>
-                    <?php } ?>
-                </select>   
-            </div>
-          </div>
+</div>
+    
+<div class="row-fluid">
           <div class="control-group">
             <div class="controls">
               <button type="submit" class="btn btn-large btn-success">
@@ -115,5 +85,83 @@ $a = new Attivita(@$_GET['id']);
               </button>
             </div>
           </div>
+        </div>
+</div>
+    
+<!--Mappa-->    
+<div class="span5">
+
+    <div class="span12">
+        <h2>
+            <i class="icon-map-marker"></i>
+            Localizza l'attività
+        </h2>
+    </div>
+    <div class="span12">
+        <div class="alert alert-info">
+            <i class="icon-info-sign"></i> <strong>Croce Rossa si muove.</strong>
+            È importante localizzare un'attività così che questa possa essere visualizzata sulla mappa, ricercata per località o vicinanza.
+        </div>
+    </div>
+
+<hr />
+
+<div class="row-fluid">
+    
+    <div class="span12">
+        <h2>
+            1. <i class="icon-search"></i> Ricerca
+        </h2>
+        
+        <hr />
+        
+        <p>Inserisci un indirizzo geografico.</p>
+        
+        <div class="input-append">
+            <input type="text" id="ricercaLuogo" class="input-large" autofocus placeholder="es.: Via Massimo, 50 Roma" />
+            <button class="btn btn-success">
+                <i class="icon-search"></i>
+            </button>
+        </div>
+    </div>
+    
+    
+    <div class="span12">
+        <h2>
+            2. <i class="icon-list"></i> Seleziona
+        </h2>
+        
+        <hr />
+        
+        <p>Seleziona un risultato tra i seguenti:</p>
+        
+        <div id="quiRisultati">
+            <div class="alert alert-info">
+                <i class="icon-info-sign"></i>
+                Ancora nessuna ricerca effettuata.
+            </div>
+        </div>
+    </div>
+
+        <h2>
+            3. <i class="icon-check"></i> Controlla
+        </h2>
+        
+        <hr />
+        
+        <p>Controlla che la posizione sulla mappa sia corretta:</p>
+        
+        <div class=".mappaGoogle" id="mappaGeografica" style="height: 500px; width: 100%;"></div>
+        
+        <hr />
+        <form action="?p=attivita.localita.ok" method="POST">
+            <input type="hidden" id="formattato" name="formattato" value="" />
+            <button id="pulsanteOk" type="submit" class="btn btn-block btn-success btn-large disabled" disabled="disabled">
+                <i class="icon-ok"></i> Accetta questa posizione
+            </button>
+            
         </form>
-</div></div>
+    </div>
+  
+</div>
+</div>
