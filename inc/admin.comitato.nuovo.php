36d2a2643047ff43db64196d8cc096ae31b63619
Federico D'Urso <ico88@hotmail.com>
2013-03-30 17:24:11 +0100

    Gestione dei comitati all'interno di GAIA

---
+++ inc/admin.comitato.nuovo.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * ©2013 Croce Rossa Italiana
+ */
+
+paginaAdmin()
+
+?>
+<div class="row-fluid">
+            <h2><i class="icon-chevron-right muted"></i> Crea nuovo Comitato</h2>
+            <div class="alert alert-block alert-info ">
+                <div class="row-fluid">
+                    <span class="span7">
+                        <p>Con questo modulo si possono aggiungere i Comitati al DB di GAIA</p>
+                        <p>Es: Comitato Provinciale di Catania ~ Catania</p>
+                    </span>
+                </div>
+            </div>           
+        </div>
+<form class="form-horizontal" action="?p=admin.comitato.nuovo.ok" method="POST">
+<div class="control-group">
+        <label class="control-label" for="inputNome">Nome nuovo Comitato </label>
+        <div class="controls">
+            <input class="input-xxlarge" type="text" name="inputNome" id="nome" placeholder="Comitato Provinciale di Catania ~ Catania" required>
+            </div>
+          </div>
+    <div class="control-group">
+            <div class="controls">
+              <button type="submit" class="btn btn-large btn-success">
+                  <i class="icon-ok"></i>
+                  Crea nuovo Comitato
+              </button>
+            </div>
+          </div>
+
