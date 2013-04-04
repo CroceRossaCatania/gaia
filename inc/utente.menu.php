1b69b456ae25079f2ca9e291fd71c98c6874ef49
Federico D'Urso <ico88@hotmail.com>
2013-04-04 15:31:22 +0200

    Inclusione riserve nel menù volontario

--- inc/utente.menu.php
+++ inc/utente.menu.php
@@ -20,7 +20,9 @@ $menu = [
     ],
     'Segreteria'      =>  [
         'utente.trasferimento'  =>  '<i class="icon-arrow-right"></i> Trasferimenti',
+        'utente.riserva'  =>  '<i class="icon-pause"></i> Riserva',
         'utente.rubricaReferenti'  =>  '<i class="icon-book"></i> Rubrica'
+       
         
     ],
     'Curriculum'    =>  [
