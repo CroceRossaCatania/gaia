a9e61c249f1ca5a5053713c3fa17e36c3a59986e
Federico D'Urso <ico88@hotmail.com>
2013-04-08 21:15:00 +0200

    Migliorata ricerca per titoli e invio di mail di massa

--- inc/presidente.titoli.ricerca.ok.php
+++ inc/presidente.titoli.ricerca.ok.php
@@ -21,7 +21,6 @@ $t = TitoloPersonale::filtra([['titolo',$f]]);
 <hr />
 <table class="table table-striped table-bordered" id="tabellaUtenti">
     <thead>
-        <th>#</th>
         <th>Nome</th>
         <th>Cognome</th>
         <th>Codice Fiscale</th>
@@ -33,15 +32,20 @@ $t = TitoloPersonale::filtra([['titolo',$f]]);
 <?php
 if($me->presiede()){
   foreach($me->presidenziante() as $appartenenza){
-             $c=$appartenenza->comitato()->id;     
-  foreach ( $t as $_t ) { 
+             $c=$appartenenza->comitato()->id;   ?>
+             <tr class="success">
+                <td colspan="7" class="grassetto">
+                    <?php echo $appartenenza->comitato()->nome; ?>
+                </td>
+            </tr>
+ <?php 
+    foreach ( $t as $_t ) { 
       $a=$_t->volontario()->id;
       $a = Appartenenza::filtra([['volontario',$a],['comitato',$c]]);
       if($a[0]!=''){
         if($_t->pConferma!=''){
             $_v = $_t->volontario();  // Una volta per tutte ?> 
             <tr>
-                <td><?php echo $_t->id; ?></td>
                 <td><?php echo $_v->nome; ?></td>
                 <td><?php echo $_v->cognome; ?></td>
                 <td><?php echo $_v->codiceFiscale; ?></td>
@@ -56,22 +60,11 @@ if($me->presiede()){
                 </td>
             </tr>
             
-    <?php }}} ?>
-<tr>
-                 <td colspan="8">
-                     <a type="submit" href="?p=utente.mail.nuova&mass&t=<?php echo $f; ?>"  class="btn btn-block btn-success">
-                        <i class="icon-envelope"></i>
-                        Invia mail a tutti
-                     </a>
-                 </td>
-             </tr>
-
-<?php }}elseif($me->admin()){
+    <?php }}}}}elseif($me->admin()){
   foreach ( $t as $_t ) { 
     if($_t->pConferma!=''){
             $_v = $_t->volontario();  // Una volta per tutte ?> 
             <tr>
-                <td><?php echo $_t->id; ?></td>
                 <td><?php echo $_v->nome; ?></td>
                 <td><?php echo $_v->cognome; ?></td>
                 <td><?php echo $_v->codiceFiscale; ?></td>
@@ -85,8 +78,9 @@ if($me->presiede()){
                     </a>
                 </td>
             </tr>
-    <?php }} ?>
-           <tr>
+    <?php }}}
+?>
+             <tr>
                  <td colspan="8">
                      <a type="submit" href="?p=utente.mail.nuova&mass&t=<?php echo $f; ?>"  class="btn btn-block btn-success">
                         <i class="icon-envelope"></i>
@@ -94,8 +88,5 @@ if($me->presiede()){
                      </a>
                  </td>
              </tr>
-    
-<?php }
-?>
  
 </table>
