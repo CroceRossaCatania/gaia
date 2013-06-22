016bb8f8b7e76095ea09ba365e001efc72c067e0
Federico D'Urso <ico88@hotmail.com>
2013-06-22 18:16:46 +0200

    Email di servizio fix #173

--- inc/utente.email.php
+++ inc/utente.email.php
@@ -19,11 +19,23 @@ paginaPrivata();
             <i class="icon-save"></i> <strong>Indirizzo salvato</strong>.
             Le comunicazioni verranno ora inviate alla nuova email.
         </div>
-        <?php } elseif ( isset($_GET['e']) )  { ?>
+        <?php } elseif ( isset($_GET['ep']) )  { ?>
         <div class="alert alert-block alert-error">
             <h4><i class="icon-exclamation-sign"></i> Email già in uso</h4>
             <p>L'email che hai inserito risulta già in uso.</p>
             <p>Ti preghiamo di inserire il tuo indirizzo email personale.</p>
+        </div>
+        <?php }elseif ( isset($_GET['ei']) )  { ?>
+        <div class="alert alert-block alert-error">
+            <h4><i class="icon-exclamation-sign"></i> Email istituzionale già in uso</h4>
+            <p>L'email istituzionale che hai inserito risulta già in uso.</p>
+            <p>Ti preghiamo di verificare il tuo indirizzo email istituzionale.</p>
+        </div>
+        <?php }elseif ( isset($_GET['ee']) )  { ?>
+        <div class="alert alert-block alert-error">
+            <h4><i class="icon-exclamation-sign"></i> Email già in uso</h4>
+            <p>Le email che hai inserito risultano già in uso.</p>
+            <p>Ti preghiamo di verificari i tuoi indirizzi email.</p>
         </div>
         <?php } else { ?>
             <hr />
@@ -36,11 +48,20 @@ paginaPrivata();
                     <p>Questa email è quella che <strong>usi per accedere</strong> ed è<br />
                        dove ti invieremo <strong>tutte le comunicazioni importanti</strong>.</p>
                 </div>
-                <label class="control-label" for="inputEmail">Email</label>
-                <div class="controls">
-                    <input type="email" autofocus name="inputEmail" id="inputEmail" required value="<?php echo $me->email; ?>">
+                <div class="control-group">
+                    <label class="control-label" for="inputEmail">Email</label>
+                    <div class="controls">
+                        <input type="email" autofocus name="inputEmail" id="inputEmail" required value="<?php echo $me->email; ?>">
+                    </div>
                 </div>
-              
+                <?php if ($me->volontario()){ ?>
+                <div class="control-group">
+                    <label class="control-label" for="inputemailServizio">Email di servizio</label>
+                    <div class="controls">
+                        <input type="email" autofocus name="inputemailServizio" id="inputemailServizio" required value="<?php echo $me->emailServizio; ?>">
+                    </div>
+                </div>
+                <?php } ?>
             <div class="form-actions">
                 <button type="submit" class="btn btn-success btn-large">
                     <i class="icon-save"></i>
