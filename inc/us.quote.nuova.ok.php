51e7367f256808f6e34797d1a85cd841395a7db4
ico88 <ico88@hotmail.com>
2013-06-26 17:22:27 +0200

    Miglioramenti ufficio soci

--- inc/us.quote.nuova.ok.php
+++ inc/us.quote.nuova.ok.php
@@ -14,7 +14,6 @@ $r = $_GET['radio'];
                          if ($app->attuale()) 
                                     {
                                 $t = new Quota();
-                                $t->volontario = $id;
                                 $t->appartenenza = $app;
                                 $time = DT::createFromFormat('d/m/Y', $_GET['inputData']);
                                 $t->timestamp = $time->getTimestamp();
@@ -22,12 +21,40 @@ $r = $_GET['radio'];
                                 $t->pConferma = $me;
                                 if($r=="prima"){
                                     $t->quota = QUOTA_PRIMO;
+                                    $s = QUOTA_PRIMO;
                                 }elseif($r == "rinnovo"){
                                     $t->quota = QUOTA_RINNOVO;
+                                    $s = QUOTA_RINNOVO;
                                 }elseif($r == "altro"){
                                     $t->quota = $_POST['importo'];
                                     $t->causale = $_POST['causale'];
+                                    $s = $_POST['causale'];
                                 }
+                                
+                                $p = new PDF('ricevutaquota', 'ricevuta.pdf');
+                                $p->_COMITATO = $app->comitato()->locale()->nomeCompleto();
+                                $p->_INDIRIZZO = $app->comitato()->locale()->formattato;
+                                $p->_PIVA = 1;
+                                $p->_ID = $t;
+                                $p->_NOME = $id->nome;
+                                $p->_COGNOME = $id->cognome;
+                                $p->_FISCALE = $id->codiceFiscale;
+                                $p->_NASCITA = date('d/m/Y', $id->dataNascita);
+                                $p->_LUOGO = $id->luogoNascita;
+                                $p->_QUOTA = $s;
+                                $p->_LUOGO = $app->comitato()->locale()->comune;
+                                $p->_DATA = date('d-m-Y', time());
+                                $f = $p->salvaFile();
+                                
+                                
+                                /* Invio ricevuta all'utente */
+                                
+                                $m = new Email('ricevutaQuota', 'Ricevuta versamento Quota');
+                                $m->a = $id;
+                                $m->da = $me;
+                                $m->_NOME       = $id->nome;
+                                $m->allega($f);
+                                $m->invia();
                                 
                                 redirect('us.quoteNo&ok');
                          }
