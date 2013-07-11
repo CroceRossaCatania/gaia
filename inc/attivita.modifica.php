7635e6f88244acc348582cb6f8f99ef00af781ae
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2013-07-11 12:40:29 +0200

    Possibilità di modificare la posizione attività una volta fissata

--- inc/attivita.modifica.php
+++ inc/attivita.modifica.php
@@ -104,7 +104,11 @@ $domini     = $me->dominiDelegazioni(APP_ATTIVITA);
         
         <p>
             <strong>Posizione geografica</strong><br />
-            <?php echo $a->luogo; ?>
+            <?php echo $a->luogo; ?><br />
+            <a href='?p=attivta.localita&id=<?= $a->id; ?>'>
+                <i class='icon-pencil'></i>
+                modifica la località
+            </a>
         </p>
         
         
