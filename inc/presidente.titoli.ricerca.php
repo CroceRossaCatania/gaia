8aa597c11a9c6fe1bb250f2ff483c4a0b7ae29c1
Federico D'Urso <ico88@hotmail.com>
2013-07-11 15:57:22 +0200

    Rimodernata ricerca per titoli fix #208
    Possibilità di uso anche da ufficio soci e centrale operativa, generazione file excel per titolo

--- inc/presidente.titoli.ricerca.php
+++ inc/presidente.titoli.ricerca.php
@@ -11,15 +11,13 @@ if (isset($_GET['t'])) {
 }
 $titoli = $conf['titoli'][$t];
 
-paginaPrivata();
+paginaApp([APP_CO,APP_PRESIDENTE,APP_SOCI]);
  
 ?>
 <div class="row-fluid">
     <div class="span12">
         
         <div id="step1">
-            
-
             <div class="alert alert-block alert-success" >
                 <div class="row-fluid">
                     <span class="span3">
@@ -29,15 +27,13 @@ paginaPrivata();
                                 <strong>Cerca</strong>
                             </span>
                         </label>
-
                     </span>
+                    
                     <span class="span9">
                         <input type="text" autofocus required id="cercaTitolo" placeholder="Cerca un titolo..." class="span12" />
                     </span>
                 </div>
-
             </div>
-
             <table class="table table-striped table-condensed table-bordered" id="risultatiRicerca" style="display: none;">
                 <thead>
                     <th>Nome risultato</th>
@@ -46,27 +42,12 @@ paginaPrivata();
                 <tbody>
 
                 </tbody>
-            </table>
-            
+            </table>            
           </div>
         
         <div id="step2" style="display: none;">
             <form action='?p=presidente.titoli.ricerca.ok' method="POST">
             <input type="hidden" name="idTitolo" id="idTitolo" />
-            <div class="alert alert-block alert-success">
-                
-                <hr />
-                <div class="row-fluid">
-                    <div class="span4 offset8">
-                        <button type="submit" class="btn btn-success">
-                            <i class="icon-plus"></i>
-                                Aggiungi il titolo
-                        </button>
-                    </div>
-                </div>
-                
-            </div>
-            
         </div>    
     </div>
 </div>
