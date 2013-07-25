9e75c8d5839b7a16e7f6eb20a123c10667bab1da
Federico D'Urso <ico88@hotmail.com>
2013-07-26 00:55:07 +0200

    Rimozione da parte del referente di persone in turno fix #243
    Solo se il turno non è finito o non è iniziato si può rimuovere la persona

---
+++ inc/attivita.modifica.volontario.rimuovi.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * ©2013 Croce Rossa Italiana
+ */
+
+paginaPrivata();
+$turno = $_GET['turno'];
+$turno = new Turno($turno);
+$a = $turno->attivita();
+paginaAttivita($a);
+
+
+$v = $_GET['v'];
+$v = new Volontario($v);
+
+$p = Partecipazione::filtra([['turno', $turno],['volontario', $v]]);
+$p[0]->cancella();
+
+$m = new Email('rimossoattivita', "Partecipazione  {$a->nome}");
+$m->a               = $v;
+$m->da              = $me;
+$m->_NOME           = $v->nomeCompleto();
+$m->_AUTORE         = $me->nomeCompleto();
+$m->_ATTIVITA       = $a->nome;
+$m->_TURNO          = $turno->nome;
+$m->_DATA           = $turno->inizio()->inTesto();
+$m->_LUOGO          = $a->luogo;
+$m->_REFERENTE      = $a->referente()->nomeCompleto();
+$m->_CELLREFERENTE  = $a->referente()->cellulare();
+$m->invia();
+
+redirect("attivita.scheda&id={$a->id}&turno={$turno->id}&riapri={$turno->id}");
