513492cb48c20cddd521a488d657492a5b4db7b6
PaoloGiustiniani <giustiniani.paolo@gmail.com>
2013-07-25 20:57:18 +0200

    Update public.sicurezza.php

--- inc/public.sicurezza.php
+++ inc/public.sicurezza.php
@@ -6,8 +6,8 @@
         
         <h3>Sicurezza del sistema</h3>
         <p>Prima di effettuare qualsiasi accesso a Gaia dovrai verificare di essere correttamente collegato al nostro sito web. </p>
-        <p>Come puoi fare questo?</p>
-        <p>E' molto semplice, prendi visione dell'URL presente nella barra degli indirizzi del tuo browser, se quest'ultimo inizierà con http<strong>s<strong>:// potrai procedere senza problemi.  In caso contrario ti chiediamo di non effettuare il login e di segnalarci(link all'email) immediatamente quanto riscontrato.</p> 
+        <p><strong>Come puoi fare questo?</strong></p>
+        <p>E' molto semplice, prendi visione dell'URL presente nella barra degli indirizzi del tuo browser, se quest'ultimo inizierà con http<strong>s</strong>:// potrai procedere senza problemi.  In caso contrario ti chiediamo di non effettuare il login e di segnalarci(link all'email) immediatamente quanto riscontrato.</p> 
         <p>Perchè ti chiediamo questo?</p> 
         <p>Per Gaia la sicurezza è tutto.</p> 
         <p>Attraverso una connessione sicura <em>(Secure Sockets Layer)</em> i tuoi dati saranno al protetto.</p>
