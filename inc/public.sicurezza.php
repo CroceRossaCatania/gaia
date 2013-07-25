d525f747db0a49625bf64870d999955eff0b10ea
PaoloGiustiniani <giustiniani.paolo@gmail.com>
2013-07-25 20:43:28 +0200

    Update public.sicurezza.php - prima parte

--- inc/public.sicurezza.php
+++ inc/public.sicurezza.php
@@ -1,12 +1,18 @@
 <div class="row-fluid">
     <div class="span12">
-        <h2><i class="icon-lock"></i> Sicurezza progetto Gaia </h2>
+        <h2><i class="icon-lock"></i>Il cuore di Gaia è la sicurezza.</h2>
         <hr />
         <p>I tuoi dati personali sono molto importanti per noi, attraverso questa pagina potrai comprendere come vengono protetti. </p>
         
-        <h3>Sicurezza Fisica</h3>
-        <p>Testo</p>
-        <p>Testo</p>
+        <h3>Sicurezza del sistema</h3>
+        <p>Prima di effettuare qualsiasi accesso a Gaia dovrai verificare di essere correttamente collegato al nostro sito web. </p>
+        <p>Come puoi fare questo?</p>
+        <p>E' molto semplice, prendi visione dell'URL presente nella barra degli indirizzi del tuo browser, se quest'ultimo inizierà con http<strong>s<strong>:// potrai procedere senza problemi.  In caso contrario ti chiediamo di non effettuare il login e di segnalarci(link all'email) immediatamente quanto riscontrato.</p> 
+        <p>Perchè ti chiediamo questo?</p> 
+        <p>Per Gaia la sicurezza è tutto.</p> 
+        <p>Attraverso una connessione sicura <em>(Secure Sockets Layer)</em> i tuoi dati saranno al protetto.</p>
+        <p>Questo tipo di comunicazione garantisce che solamente il client ed il server (mediante certificati di sicurezza) siano in grado di conoscere il contenuto della comunicazione inviata.</p>
+        <p>Il certificato che ti proteggerà è erogato da <strong>GlobalSign Root CA.</strong></p>
         <p>&nbsp;</p>
         
         <h3>Sicurezza Dati</h3>
