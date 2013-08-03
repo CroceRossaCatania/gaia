bed9de36c03b1985ff2a43f7c01e7588c12a5093
luca-dex <luca.desano@gmail.com>
2013-08-03 15:38:50 +0200

    creazione branch stat - menu statistiche

---
+++ inc/utente.statistiche.volontari.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * ©2013 Croce Rossa Italiana
+ */
+
+paginaPrivata();
+
+
+?>
+<div class="row-fluid">
+    <div class="span3">
+        <?php        menuVolontario(); ?>
+    </div>
+    <div class="span6">
+        <h2><i class="icon-puzzle-piece"></i> Statistiche Volontari</h3>
+        
+        
+        
+    </div>
+</div>
