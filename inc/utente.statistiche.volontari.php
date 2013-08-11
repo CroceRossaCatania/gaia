d2725561e52b936e111c3dfd98a2bd10ee4d6edb
luca-dex <luca.desano@gmail.com>
2013-08-11 15:35:35 +0200

    bug fix

--- inc/utente.statistiche.volontari.php
+++ inc/utente.statistiche.volontari.php
@@ -5,10 +5,11 @@
  */
 
 paginaPrivata();
+caricaSelettoreComitato();
 
 
 ?>
-       
+
 
 <div class="row-fluid">
     <div class="span3">
@@ -16,21 +17,42 @@ paginaPrivata();
     </div>
     <div class="span9">
         <h2><i class="icon-puzzle-piece"></i> Statistiche Volontari</h2>
-        <?php
-                $comitato = $me->unComitato();
-                $info = $comitato->informazioniVolontariJSON()
-        ?>
         <div class="row-fluid">
-            <div id="graficosx" class="span6"></div>   
-			<div id="graficodx" class="span6"></div>
-    	</div>
-        <div class="row-fluid">
-            <div id="graficoanz" class="span12"></div>   
-        </div>
-        <script type="text/javascript">
-            volontari(<?php echo($info); ?>)
-        </script>            
+        <form class="form-horizontal" action="?p=utente.statistiche.volontari.ok&id=<?php echo $me->id; ?>" method="POST">
+            <div class="control-group">
+                <label class="control-label" for="inputComitato">Scegli il comitato</label>
+                <div class="controls">
+                    <a class="btn btn-inverse" data-selettore-comitato="true" data-input="inputComitato">
+                        Seleziona un comitato... <i class="icon-pencil"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                  <button type="submit" class="btn btn-large btn-success">
+                      <i class="icon-ok"></i>
+                      Visualizza dati
+                  </button>
+              </div>
+          </div>
+      </div>
+
+
+      <?php
+      $comitato = $me->unComitato();
+      $info = $comitato->informazioniVolontariJSON()
+      ?>
+      <div class="row-fluid">
+        <div id="graficosx" class="span6"></div>   
+        <div id="graficodx" class="span6"></div>
     </div>
+    <div class="row-fluid">
+        <div id="graficoanz" class="span12"></div>   
+    </div>
+    <script type="text/javascript">
+        volontari(<?php echo($info); ?>)
+    </script>            
+</div>
 </div>
 
 
