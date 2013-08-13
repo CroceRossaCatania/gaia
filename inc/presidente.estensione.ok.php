d63d4fc3f6be30f70716752f92cbae08eb15db82
luca-dex <luca.desano@gmail.com>
2013-08-13 10:12:03 +0200

    Concessione estensioni funzionante ref #263

--- inc/presidente.estensione.ok.php
+++ inc/presidente.estensione.ok.php
@@ -11,68 +11,24 @@
 
     if (isset($_GET['si'])) {
         $v = $e->volontario()->id;
-        $e->stato = EST_OK;
-        $e->pConferma = $me->id;
-        $e->tConferma = time();
-        $e = Appartenenza::filtra([
-            ['volontario', $v],
-            ['stato', MEMBRO_VOLONTARIO]
-            ]);
-        foreach ($e as $a ) { 
+        $e->concedi();
+        $a = $e->appartenenza;
+        
+        $a = new Appartenenza($a);
 
-            /*
-            Bisogna ancora fare in modo che vada a finalizzare la nuova appartenenza senza fare altre cose strane.
+        $a->timestamp = time();
+        $a->conferma  = $me->id;    
+        $a->stato = MEMBRO_ESTESO;
 
-            */
-            if ($a->attuale()) 
-            {
-                $a = new Appartenenza($a);
-                $a->timestamp = time();
-                $a->conferma  = $me->id;    
-                $a->fine = time();
-                /*Eliminazione iscrizione gruppo di lavoro*/
-                $g = AppartenenzaGruppo::filtra([
-                    ['volontario', $v],
-                    ['appartenenza',$a]
-                    ]);
-                foreach($g as $_g){
-                    $_g = new AppartenenzaGruppo($_g);
-                    $_g->fine = time();                                    
-                }
-                /*Terminazione riserve in sospeso*/
-                $r = Riserva::filtra([
-                    ['volontario', $v], 
-                    ['appartenenza',$a]
-                    ]);
-                $r = new Riserva($r[0]);
-                $r->fine = time();
-                $r->stato = RISERVA_SCAD;
-            }
-        }
-        $t = Appartenenza::filtra([
-            ['volontario', $v],
-            ['stato', TRASF_INCORSO]
-            ]);
-        foreach ($t as $a ) { 
-            if ($a->attuale()) 
-            {
-                $a = new appartenenza($a);
-                $a->timestamp = time();
-                $a->stato     = MEMBRO_PENDENTE;
-                $a->conferma = $me->id;
-                $a->inizio = time();
-                $a->fine = PROSSIMA_SCADENZA;
-
-                $m = new Email('richiestaTrasferimentook', 'Richiesta trasferimento approvata: ' . $a->comitato()->nome);
-                $m->da = $me; 
-                $m->a = $a->volontario();
-                $m->_NOME       = $a->volontario()->nome;
-                $m->_COMITATO   = $a->comitato()->nomeCompleto();
-                $m-> _TIME = date('d-m-Y', $t->protData);
-                $m->invia();
-            }
-        }
-        redirect('presidente.trasferimento&ok');  
+        $m = new Email('richiestaEstensioneok', 'Richiesta estensione approvata: ' . $a->comitato()->nome);
+        $m->da = $me; 
+        $m->a = $a->volontario();
+        $m->_NOME       = $a->volontario()->nome;
+        $m->_COMITATO   = $a->comitato()->nomeCompleto();
+        $m-> _TIME = date('d-m-Y', $e->protData);
+        $m->invia();
+      
+        redirect('presidente.estensione&ok');  
     }
 
     if (isset($_GET['no'])) {
@@ -87,7 +43,7 @@
         $a->inizio = time();
         $a->fine = time();
 
-        $m = new Email('richiestaTrasferimentono', 'Richiesta trasferimento negata: ' . $a->comitato()->nome);
+        $m = new Email('richiestaEstensioneno', 'Richiesta estensione negata: ' . $a->comitato()->nome);
         $m->da = $me;   
         $m->a = $a->volontario();
         $m->_NOME       = $a->volontario()->nome;
@@ -96,6 +52,6 @@
         $m-> _MOTIVO = $_POST['motivo'];
         $m->invia();
 
-        redirect('presidente.trasferimento&no');   
+        redirect('presidente.estensione&no');   
     }
 ?>
