a7377f3073061bcde4490fa2cea94ba6f907f81a
Federico D'Urso <ico88@hotmail.com>
2013-08-16 10:56:32 +0200

    Schermata di selezione

--- inc/us.utente.trasferisci.php
+++ inc/us.utente.trasferisci.php
@@ -6,33 +6,40 @@
 
 paginaApp([APP_SOCI, APP_PRESIDENTE]);
 caricaSelettore();
+caricaSelettoreComitato();
 paginaModale();
 
 ?>
 
-<input type="hidden" name="id"  />
-
-<div class="modal fade automodal">
+<form action="?p=us.utente.trasferisci.ok" method="POST">
+    <div class="modal fade automodal">
         <div class="modal-header">
           <h3><i class="icon-arrow-right"></i> Trasferisci volontario</h3>
         </div>
         <div class="modal-body">
-          <p><strong>Trasferimento</strong>
            <p> Con questo modulo potrai trasferire un volontario presso un altro Comitato </p>
+           <p class="text-error"><i class="icon-warning-sign"></i> Attenzione questa operazione non è reversibile </p>
            <p>&nbsp;</p>
           <p>
-              <a data-selettore="true" data-input="inputReferente" data-autosubmit="true" 
+              <a data-selettore="true" data-input="inputReferente" data-autosubmit="false" 
                  class="btn btn-inverse btn-block btn-large">
                   Seleziona un volontario... <i class="icon-pencil"></i>
               </a>
           </p>
+          <p>
+                <a class="btn btn-inverse btn-block btn-large" data-selettore-comitato="true" data-input="inputComitato">
+                    Seleziona un comitato di destinazione... <i class="icon-pencil"></i>
+                </a>
+          </p>
+          <p class="allinea-centro">
+                 <input class="span6 allinea-centro" type="text" name="inputMotivo" id="motivo" placeholder="Motivazione" required>
+          </p>
         </div>
         <div class="modal-footer">
           <a href="?p=us.dash" class="btn">Annulla</a>
-         <!-- <button type="submit" class="btn btn-primary">
-              <i class="icon-asterisk"></i> Crea attività
-          </button>-->
+         <button type="submit" class="btn btn-primary">
+              <i class="icon-arrow-right"></i> Trasferisci Volontario
+          </button>
         </div>
-</div>
-    
+    </div>
 </form>
