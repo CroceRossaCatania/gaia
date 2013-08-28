7720c1ec5afada051ac19b96253b8329938f3ee7
Federico D'Urso <ico88@hotmail.com>
2013-08-28 21:25:55 +0200

    Pagina contenente tutti gli script per gaia

---
+++ inc/admin.script.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * ©2013 Croce Rossa Italiana
+ */
+
+paginaAdmin();
+
+?>
+<div class="row-fluid">
+    <div class="row-fluid">
+        <div class="span12">
+            <div class="span12 centrato">
+                <h1><i class="icon-stackexchange"></i> Script</h1>
+                <div class="alert alert-block alert-danger">
+                    <h3><i class="icon-warning-sign"></i> Attenzione!!!</h3>
+                    <p>Pagina contenente gli script di gaia</p>
+                    <p>Prestare massima attenzione!!!</p>
+                </div>        
+            </div>
+            <hr/>
+        </div>
+    </div>
+
+    <div class="row-fluid">
+        <div class="span6 centrato">
+            <div class='alert alert-info'>
+                <strong>Script per il pagamento delle quote associative</strong>
+                <p>Questo script paga tutte le quote associative dell'anno corrente</p>
+            </div>
+            <a href="?p=admin.quotepaga" class="btn btn-large btn-info">
+                <i class="icon-money"></i>
+                Paga quote associative
+            </a>
+        </div>
+        
+        <div class="span6 centrato">
+            <div class="alert alert-success">
+                <strong>Script per il popolamento del campo sesso</strong>
+                <p>Questo script assegna il sesso agli utenti di gaia sulla base del loro codice fiscale</p>
+            </div>
+            <a href="?p=admin.sesso" class="btn btn-large btn-success">
+                <i class="icon-male"></i><i class="icon-female"></i>
+                Sesso
+            </a>
+        </div>
+    </div>
+</div>
