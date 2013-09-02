b3165d950b2c30e8e70f2f3ab8e47e3e1d82c316
Federico D'Urso <ico88@hotmail.com>
2013-09-02 20:45:02 +0200

    Report turni per volontari appartenenti al gruppo di lavoro

--- inc/gruppo.utente.report.php
+++ inc/gruppo.utente.report.php
@@ -3,25 +3,20 @@
 /*
  * ©2013 Croce Rossa Italiana
  */
-
-paginaApp([APP_OBIETTIVO , APP_PRESIDENTE]);
-
-$elenco = $me->comitatiApp ([ APP_OBIETTIVO, APP_PRESIDENTE ]);
-foreach ( $elenco as $unit ){
-	$t[] = $unit->locale();
-}
-$t = array_unique($t);
+paginaPrivata();
+$id = $_GET['id'];
 
 ?>
+<script type="text/javascript"><?php require './js/obiettivo.report.reperibilita.js'; ?></script>
 <div class="row-fluid">
-	<form class="form-horizontal" action="?p=obiettivo.report.reperibilita.ok" method="POST">
+	<form class="form-horizontal" action="?p=gruppo.utente.report.ok&id=<?php echo $id; ?>" method="POST">
 		<div class="span12">
 			<div class="row-fluid">
-				<h2><i class="icon-time muted"></i> Genera report delle reperibilità</h2>
+				<h2><i class="icon-time muted"></i> Genera report dei turni</h2>
 				<div class="alert alert-block alert-info ">
 					<div class="row-fluid">
 						<span class="span12">
-							<p>Con questo modulo si può generare il report delle reperibilità all'interno di un Comitato.</p>
+							<p>Con questo modulo si può generare il report dei turni eseguiti dal volontario.</p>
 							<p>Specificare la data di inizio e di fine per generare un report di quell'intervallo. </p>
 						</span>
 					</div>
@@ -29,18 +24,6 @@ $t = array_unique($t);
 			</div>
 
 			<div class="row-fluid">
-				<div class="control-group">
-						<label class="control-label" for="oid">Seleziona Comitato</label>
-						<div class="controls">
-							<select class="input-xxlarge" id="oid" name="oid" required>
-								<?php
-								foreach ( $t as $numero ) { ?>
-								<option value="<?php echo $numero->oid(); ?>"><?php echo $numero->nomeCompleto(); ?></option>
-								<?php } ?>
-							</select>   
-						</div> 
-					</div>
-
 					<div class="control-group">
 						<label class="control-label" for="datainizio">Data inizio </label>
 						<div class="controls">
