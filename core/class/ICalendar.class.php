46b95760e76ea5e308676f88859de334098b60bc
luca-dex <luca.desano@gmail.com>
2013-09-08 22:59:22 +0200

    mmm.. ultimo tentativo prima della disfatta

--- core/class/ICalendar.class.php
+++ core/class/ICalendar.class.php
@@ -17,28 +17,25 @@ class ICalendar extends File {
         /* Strutturo il file */
         $this->autore  = $sessione->utente;
         $this->nome    = $name;
-        $this->mime    = 'text/calendar; charset=UTF-8; method=REQUEST';
+        $this->mime    = 'text/calendar; method=REQUEST';
 
         /* Inserisco le informazioni */
-        $s = "
-        BEGIN:VCALENDAR
-        VERSION:2.0
-        PRODID:-//Croce Rossa Italiana//Progetto GAIA//IT
-        CALSCALE:GREGORIAN
-        METHOD:REQUEST
-        BEGIN:VEVENT
-        DTSTAMP:".date('Ymd\THis\Z', time())."
-        DTSTART:".date('Ymd\THis\Z', $turno->inizio)."
-        DTEND:".date('Ymd\THis\Z', $turno->fine)."
-        SUMMARY:".$att->nome.": ".$turno->nome."  
-        LOCATION:".$att->luogo."
-        UID:".$turno->id."
-        DESCRIPTION:\nTurno organizzato da".$c->nomeCompleto().",
-        dettagli: ".strip_tags($att->descrizione)."
-        ORGANIZER;CN=".$ref->nomeCompleto().":mailto:".$ref->email."
-        END:VEVENT
-        END:VCALENDAR
-        ";
+        $s = "BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Croce Rossa Italiana//Progetto GAIA//IT
+METHOD:REQUEST
+BEGIN:VEVENT
+DTSTAMP:".date('Ymd\THis\Z', time())."
+DTSTART:".date('Ymd\THis', $turno->inizio)."
+DTEND:".date('Ymd\THis', $turno->fine)."
+SUMMARY:".$att->nome.": ".$turno->nome."
+LOCATION:".$att->luogo."
+UID:".$turno->id."
+DESCRIPTION:\nTurno organizzato da ".$c->nomeCompleto().",
+dettagli: ".strip_tags($att->descrizione)."
+ORGANIZER;CN=ORGANIZER:mailto:".$ref->email."
+END:VEVENT
+END:VCALENDAR\n";
 
         file_put_contents($this->percorso(), $s);
     }    
