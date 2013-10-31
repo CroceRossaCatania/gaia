e3079c82d76b0c30ff997c31b38019c1ef2b6489
Federico <ico88@hotmail.com>
2013-10-31 22:55:12 +0100

    Se cancello appartenenza, cancello anche riserve/estensioni/trasferimenti ad essa collegati fix #391

--- inc/us.appartenenza.cancella.php
+++ inc/us.appartenenza.cancella.php
@@ -10,6 +10,18 @@ $a = $_GET['a'];
 
 $app = Appartenenza::id($a);
 $v = $app->volontario;
+
+foreach(Estensione::filtra([['appartenenza', $app]])) as $estensione){
+	$estensione->cancella();
+}
+
+foreach(Trasferimento::filtra([['appartenenza', $app]])) as $trasferimento){
+	$trasferimento->cancella();
+}
+foreach(Riserva::filtra([['appartenenza', $app]])) as $riserva){
+	$riserva->cancella();
+}
+
 $app->cancella();
 
 redirect('presidente.utente.visualizza&id=' . $v);
