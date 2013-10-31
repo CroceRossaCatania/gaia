bb94f4000b7b88225e17f553c0b87b59496bf46a
Federico <ico88@hotmail.com>
2013-10-31 09:55:14 +0100

    Presidente visualizza consenso ref #333

--- inc/presidente.utente.visualizza.php
+++ inc/presidente.utente.visualizza.php
@@ -168,6 +168,12 @@ proteggiDatiSensibili($u, [APP_SOCI, APP_PRESIDENTE]);
                 </select>   
             </div>
           </div>
+          <div class="control-group input-prepend">
+            <label class="control-label" for="inputConsenso">Consenso dati personali</label>
+            <div class="controls">
+             <input value="<?php if($u->consenso()){ echo "Acquisito";}else{ echo "Non Acquisito"; } ?>"  type="text" id="inputConsenso" name="inputConsenso" readonly/>
+            </div>
+          </div>
         <hr />
          
         <div class="form-actions">
