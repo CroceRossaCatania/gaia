784f2949596cd918cad2b8f70913ca8405b18947
Federico <ico88@hotmail.com>
2013-11-02 13:23:04 +0100

    Validazione mail seconda parte ref #618

---
+++ inc/validaEmail.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * ©2013 Croce Rossa Italiana
+ */
+
+$codice = $_GET['c'];
+
+/* Cerca codice di validazione */
+$validazione = Validazione::cercaValidazione($codice , VAL_MAIL);
+if($validazione==false){
+	redirect('&sca');
+}
+
+$validazione->stato = VAL_CHIUSO;
+
+$e = new Email('convalidaEmail', 'Email sostituita');
+$e->a = $p;
+$e->_NOME = $p->nome;
+$e->invia();
+
+redirect('login');
