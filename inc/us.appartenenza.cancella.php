0dcc13cddb5a440314637cef9fdec953813a6efb
Federico <ico88@hotmail.com>
2013-11-07 15:24:11 +0100

    Cancellazione utente e appartenenza non lascia chiavi vacanti fix #391

--- inc/us.appartenenza.cancella.php
+++ inc/us.appartenenza.cancella.php
@@ -11,8 +11,20 @@ $a = $_GET['a'];
 $app = Appartenenza::id($a);
 $v = $app->volontario;
 
+foreach(Coturno::filtra([['appartenenza', $app]]) as $coturno){
+	$coturno->cancella();
+}
+
+foreach(Dimissione::filtra([['appartenenza', $app]]) as $dimissioni){
+	$dimissioni->cancella();
+}
+
 foreach(Estensione::filtra([['appartenenza', $app]]) as $estensione){
 	$estensione->cancella();
+}
+
+foreach(Quota::filtra([['appartenenza', $app]]) as $quote){
+	$quote->cancella();
 }
 
 foreach(Trasferimento::filtra([['appartenenza', $app]]) as $trasferimento){
