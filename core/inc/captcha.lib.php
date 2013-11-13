65c1b49af48fc336db7c6ae3d91a6225fb37cb06
Alfio Emanuele <alfio.emanuele.f@gmail.com>
2013-11-13 01:05:43 +0000

    Evviva le connessioni sicure

--- core/inc/captcha.lib.php
+++ core/inc/captcha.lib.php
@@ -18,10 +18,10 @@ function captcha_mostra() {
 		die('Errore: Configurazione captcha mancante.');
 	echo <<<RECAPTCHA
 		 <script type="text/javascript"
-			       src="http://www.google.com/recaptcha/api/challenge?k={$conf['recaptcha']['public_key']}">
+			       src="https://www.google.com/recaptcha/api/challenge?k={$conf['recaptcha']['public_key']}">
 			    </script>
 			    <noscript>
-			       <iframe src="http://www.google.com/recaptcha/api/noscript?k={$conf['recaptcha']['public_key']}"
+			       <iframe src="https://www.google.com/recaptcha/api/noscript?k={$conf['recaptcha']['public_key']}"
 			           height="300" width="500" frameborder="0"></iframe><br>
 			       <textarea name="recaptcha_challenge_field" rows="3" cols="40">
 			       </textarea> 
