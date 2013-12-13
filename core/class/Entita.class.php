54218a63b6f513a84dc1d74c84149fb1cbb1b86c
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2013-12-13 23:08:14 +0000

    Ma porca troia

--- core/class/Entita.class.php
+++ core/class/Entita.class.php
@@ -457,11 +457,10 @@ class Entita {
     public static function daOid($oid) {
         $obj = explode(':', $oid);
         $cl = $obj[0];
-
-        if ( !is_a($cl, get_called_class()) )
-            throw new Errore(1013):
-
-        return new $cl($obj[1]);
+        $obj = $cl::id($obj[1]);
+        if ( ! $obj instanceOf static )
+            throw new Errore(1013);
+        return $obj;
     }
 
 }
