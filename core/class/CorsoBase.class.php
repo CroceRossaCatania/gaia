0f8a4abdc2e586ba0183f4201f4998520726a05d
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2013-12-13 22:49:35 +0000

    Corsi base p. 1

---
+++ core/class/CorsoBase.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * ©2013 Croce Rossa Italiana
+ */
+
+class CorsoBase extends Entita {
+
+    protected static
+        $_t  = 'corsibase',
+        $_dt = 'corsibase_dettagli';
+
+
+    
+}
