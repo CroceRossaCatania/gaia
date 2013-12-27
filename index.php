f7aff3d6bbce2aa55fb1efb4ceb0361c0cdca9e2
luca-dex <luca.desano@gmail.com>
2013-12-27 16:45:15 +0100

    perchè noi abbiamo alfio nel c...uore! #251

--- index.php
+++ index.php
@@ -61,7 +61,6 @@ $_descrizione = 'Crediamo in una Croce Rossa Italiana che sa muoversi velocement
 
     <!-- CSS -->
     <link href="css/bootstrap.min.css"      rel="stylesheet" media="screen">
-    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css"   rel="stylesheet" media="screen">
     <link href="css/main.css"               rel="stylesheet" media="screen">
     <link href="css/fullcalendar.css"       rel="stylesheet" media="screen">
