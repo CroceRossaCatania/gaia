9898a7c80d1c5f28e16f938b794515711105ab2b
Federico <ico88@hotmail.com>
2013-12-31 12:21:15 +0100

    Evitiamo di lasciare incarichi vacanti ref #556

--- inc/presidente.utente.dimetti.ok.php
+++ inc/presidente.utente.dimetti.ok.php
@@ -6,56 +6,124 @@
 
 paginaApp([APP_SOCI, APP_PRESIDENTE]);
 
-controllaParametri(array('id'), 'presidente.utenti&errGen');
+controllaParametri(array('id','motivo','info'), 'presidente.utenti&errGen');
 
 $v = Volontario::id($_GET['id']);
+$attuale = $v->appartenenzaAttuale();
+$comitato = $attuale->comitato();
 
-foreach ( $conf['dimissioni'] as $numero => $dimissioni ) {
-    if ( $numero == $_POST['motivo']) { 
-        $motivo =  $dimissioni;
-    } 
-}
-
+/* Avviso il volontario */
 $m = new Email('dimissionevolontario', 'Dimissione Volontario: ' . $v->nomeCompleto());
 $m->da      = $me;
-$m->a       = $v->volontario();
-$m->_NOME   = $v->volontario()->nome;
-$m->_MOTIVO = $motivo;
+$m->a       = $v;
+$m->_NOME   = $v->nome;
+$m->_MOTIVO = $conf['dimissioni'][$_POST['motivo']];
 $m->_INFO   = $_POST['info'];
 $m->invia();
-                
+
+/* Creo la dimissione */                
 $d = new Dimissione();
 $d->volontario      = $v->id;
-$d->appartenenza    = $v->appartenenzaAttuale();
-$d->comitato        = $v->appartenenzaAttuale()->comitato;
+$d->appartenenza    = $attuale;
+$d->comitato        = $comitato;
 $d->motivo = $_POST['motivo'];
 $d->info = $_POST['info'];
 $d->tConferma = time();
 $d->pConferma = $me;
 
-$a = Appartenenza::filtra([['volontario', $v]]);
-$i = Delegato::filtra([['volontario',$v]]);
-$e = Estensione::filtra([['volontario', $v], ['stato', EST_OK]]);
-$r = Riserva::filtra([['volontario', $v],['stato', RISERVA_OK]]);
-
-foreach ($i as $_i){
-    $b->fine = time();   
+/* Evitiamo di lasciare compiti a chi non è più in CRI */
+$f = Area::filtra([
+  ['responsabile', $v]
+  ]);
+foreach($f as $_f){
+    $_f->dimettiReferente();
 }
 
-foreach ($e as $_e){
-    $est->termina();
+$f = Attivita::filtra([
+  ['referente', $v]
+  ]);
+foreach($f as $_f){
+    $_f->referente = $comitato->unPresidente();
 }
 
-foreach ( $a as $_a){
-    if($_a->attuale()){
-        $_a = Appartenenza::id($_a);
-        $_a->fine = time();
-        $_a->stato = MEMBRO_DIMESSO;
-        $f = Persona::id($v);
-        $f->stato = PERSONA;
-        $f->admin=null;
-    }
+$f = Autorizzazione::filtra([
+  ['volontario', $v]
+  ]);
+foreach($f as $_f){
+    $_f->volontario = $comitato->unPresidente();
 }
-                    
+
+$f = Gruppo::filtra([
+  ['referente', $v]
+  ]);
+foreach($f as $_f){
+    $_f->referente = $comitato->unPresidente();
+}
+
+$f = AppartenenzaGruppo::filtra([
+  ['volontario', $v]
+  ]);
+foreach ($f as $_f) {
+    $_f->cancella();
+}
+
+$f = Reperibilita::filtra([
+  ['volontario', $v]
+  ]);
+foreach ($f as $_f) {
+    $_f->fine = time();
+}
+
+$f = TitoloPersonale::filtra([
+  ['volontario', $t]
+  ]);
+foreach ($f as $_f) {
+    $_f->cancella();
+}
+
+/* da rivedere da qui in poi */
+
+$f = Delegato::filtra([
+  ['volontario', $v]
+  ]);
+foreach ($f as $_f) {
+    $_f->fine = time();
+}
+
+$f = Partecipazione::filtra([
+  ['volontario', $v]
+  ]);
+foreach ($f as $_f) {
+    $_f->cancella();
+}
+
+
+$f = Estensione::filtra([
+  ['volontario', $v]
+  ]);
+foreach ($f as $_f) {
+    $_f->cancella();
+}
+
+$f = Riserva::filtra([
+  ['volontario', $v]
+  ]);
+foreach ($f as $_f) {
+    $_f->cancella();
+}
+
+$f = Trasferimento::filtra([
+  ['volontario', $v]
+  ]);
+foreach ($f as $_f) {
+    $_f->cancella();
+}
+
+/* Chiudo l'appartenenza e declasso a persona */
+$attuale->fine = time();
+$attuale->stato = MEMBRO_DIMESSO;
+$v->stato = PERSONA;
+$v->admin=null;
+               
 redirect('presidente.utenti&dim');   
 ?>
