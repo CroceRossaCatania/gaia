22310f0b5830efc7acc99c9cb4d22e4ad8c1677c
luca-dex <luca.desano@gmail.com>
2013-12-31 04:14:57 +0100

    vaso recuperato, è ancora da mettere in salvo però #418

--- inc/us.quote.nuova.ok.php
+++ inc/us.quote.nuova.ok.php
@@ -5,70 +5,79 @@
  */
 
 
-$parametri = ['id', 'inputQuota'];
+$parametri = ['id', 'inputImporto', 'inputData'];
 controllaParametri($parametri, 'us.dash&err');
 
 $id = $_GET['id'];
-$id = Volontario::id($id);
+$v = Volontario::id($id);
 
-proteggiDatiSensibili($id, [APP_SOCI , APP_PRESIDENTE]);
+proteggiDatiSensibili($v, [APP_SOCI , APP_PRESIDENTE]);
 
-$r = $_GET['inputQuota'];
+$importo = (float) $_GET['inputImporto'];
+$importo = round($importo, 2);
+if ($importo < QUOTA_ATTIVO) {
+    redirect('us.quote.nuova&id='.$id.'&importo');
+}
 
-foreach ( $id->storico() as $app ) { 
-    if ($app->attuale()) {
-        $t = new Quota();
-        $t->appartenenza = $app;
-        $time = DT::createFromFormat('d/m/Y', $_GET['inputData']);
-        $t->timestamp = $time->getTimestamp();
-        $t->tConferma = time();
-        $t->pConferma = $me;
-        $t->anno = $_GET['inputAnno'];
-        if($r==QUOTA_PRIMO){
-            $t->quota = QUOTA_PRIMO;
-            $s = QUOTA_PRIMO;
-            $i = "Versamento quota iscrizione";
-            $t->causale = $i;
-        }elseif($r == QUOTA_RINNOVO){
-            $t->quota = QUOTA_RINNOVO;
-            $s = QUOTA_RINNOVO;
-            $i = "Versamento quota di rinnovo annuale";
-            $t->causale = $i;
-        }elseif($r ==QUOTA_ALTRO){
-            $t->quota = $_GET['inputImporto'];
-            $t->causale = $_GET['inputCausale'];
-            $s = $_GET['inputImporto'];
-            $i = $_GET['inputCausale'];
+$app = $v->appartenenzaAttuale();
 
-        }
+$q = new Quota();
+$q->appartenenza = $app;
+$time = DT::createFromFormat('d/m/Y', $_GET['inputData']);
+$q->timestamp = $time->getTimestamp();
+$q->tConferma = time();
+$q->pConferma = $me;
+$q->anno = date('Y');
 
-        $p = new PDF('ricevutaquota', 'ricevuta.pdf');
-        $p->_COMITATO = $app->comitato()->locale()->nomeCompleto();
-        $p->_INDIRIZZO = $app->comitato()->locale()->formattato;
-        $iva = PIVA;
-        $p->_PIVA = $iva;
-        $p->_ID = $t;
-        $p->_NOME = $id->nome;
-        $p->_COGNOME = $id->cognome;
-        $p->_FISCALE = $id->codiceFiscale;
-        $p->_NASCITA = date('d/m/Y', $id->dataNascita);
-        $p->_LUOGO = $id->luogoNascita;
-        $p->_QUOTA = $s;
-        $p->_CAUSALE = $i;
-        $p->_LUOGO = $app->comitato()->locale()->comune;
-        $p->_DATA = date('d-m-Y', time());
-        $f = $p->salvaFile();                                
+/* 
+ * C'è molto lavoro da fare ancora
+ *
 
-        /* Invio ricevuta all'utente */
+if($r==QUOTA_PRIMO){
+    $t->quota = QUOTA_PRIMO;
+    $s = QUOTA_PRIMO;
+    $i = "Versamento quota iscrizione";
+    $t->causale = $i;
+}elseif($r == QUOTA_RINNOVO){
+    $t->quota = QUOTA_RINNOVO;
+    $s = QUOTA_RINNOVO;
+    $i = "Versamento quota di rinnovo annuale";
+    $t->causale = $i;
+}elseif($r ==QUOTA_ALTRO){
+    $t->quota = $_GET['inputImporto'];
+    $t->causale = $_GET['inputCausale'];
+    
+    $i = $_GET['inputCausale'];
 
-        $m = new Email('ricevutaQuota', 'Ricevuta versamento Quota');
-        $m->a = $id;
-        $m->da = $me;
-        $m->_NOME       = $id->nome;
-        $m->allega($f);
-        $m->invia();
+}
 
-        redirect('us.quoteNo&ok');
-    }
-} 
+$p = new PDF('ricevutaquota', 'ricevuta.pdf');
+$p->_COMITATO = $app->comitato()->locale()->nomeCompleto();
+$p->_INDIRIZZO = $app->comitato()->locale()->formattato;
+$iva = PIVA;
+$p->_PIVA = $iva;
+$p->_ID = $t;
+$p->_NOME = $id->nome;
+$p->_COGNOME = $id->cognome;
+$p->_FISCALE = $id->codiceFiscale;
+$p->_NASCITA = date('d/m/Y', $id->dataNascita);
+$p->_LUOGO = $id->luogoNascita;
+$p->_QUOTA = $s;
+$p->_CAUSALE = $i;
+$p->_LUOGO = $app->comitato()->locale()->comune;
+$p->_DATA = date('d-m-Y', time());
+$f = $p->salvaFile();                                
 
+/* Invio ricevuta all'utente */
+
+/*
+$m = new Email('ricevutaQuota', 'Ricevuta versamento Quota');
+$m->a = $id;
+$m->da = $me;
+$m->_NOME       = $id->nome;
+$m->allega($f);
+$m->invia();
+*/
+
+redirect('us.quoteNo&ok');
+    
--- inc/us.quote.nuova.php
+++ inc/us.quote.nuova.php
@@ -11,22 +11,38 @@ $v = Volontario::id($id);
 
 proteggiDatiSensibili($v, [APP_SOCI , APP_PRESIDENTE]);
 
+/* 
+ * Controllo se ordinario o attivo 
+ * e recupero valore della quota minima 
+ */
+$attivo = false;
+if ($v->stato == VOLONTARIO) {
+  $attivo = true;
+}
+$quotaMin = $attivo ? QUOTA_ATTIVO : QUOTA_ORDINARIO;
+
 ?>
 <form action="?" method="GET">
   <input type="hidden" name="p" value="us.quote.nuova.ok">
   <div class="modal fade automodal">
     <div class="modal-header">
-      <h3><i class="icon-certificate"></i> Pagamento Quota</h3>
+      <h3><i class="icon-certificate"></i> Pagamento quota <?php echo($attivo ? 'socio attivo' : 'socio ordinario') ?></h3>
     </div>
     <div class="modal-body">
       <div class="row-fluid">
         <div class="span12">
+          <?php if(isset($_GET['importo'])) { ?>
+            <div class="alert alert-error">
+              <h4><i class="icon-warning-sign"></i> Qualcosa non ha funzionato</h4>
+              <p>Sembra che tu abbia inserito un importo inferiore a <?php echo $quotaMin;?>.00 €.</p>
+            </div>
+          <?php } ?>
           <div class="alert alert-info">
             <h4>Rinnovo per <?php echo($v->nomeCompleto()); ?></h4>
             <p>Indica la data di registrazione della ricevuta e l'importo, che deve essere superiore a
-            8.00 €.</p>
+            <?php echo $quotaMin;?>.00 €.</p>
             <p>L'importo deve essere espresso in cifre, utilizzando il punto come separatore decimale e senza inserire il simbolo dell'Euro (€).
-            Ad esempio 8, 8.00 o 15.32 sono importi accettati mentre 8,0 e 15,32 <strong>non</strong> sono importi accettati.</p>
+            Ad esempio <?php echo $quotaMin;?>, <?php echo $quotaMin;?>.00 o 19.32 sono importi accettati mentre <?php echo $quotaMin;?>,0 e 19,32 <strong>non</strong> sono importi accettati.</p>
           </div>
         </div>
       </div>
@@ -56,6 +72,7 @@ proteggiDatiSensibili($v, [APP_SOCI , APP_PRESIDENTE]);
           <label class="control-label" id="importo" for="inputImporto" >Importo in €</label>
           <div class="controls">
             <input class="input-medium" type="text" name="inputImporto" id="inputImporto" >
+            &nbsp; <span class="muted"> da <?php echo $quotaMin;?>.00€ in su</span>
           </div>
         </div>
 
