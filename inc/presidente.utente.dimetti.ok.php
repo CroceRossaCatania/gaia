7e27fcb4fdea5a419e33b3958c8af0a255e9f788
Federico <ico88@hotmail.com>
2013-12-31 20:20:17 +0100

    Dimissioni serie fix #556

--- inc/presidente.utente.dimetti.ok.php
+++ inc/presidente.utente.dimetti.ok.php
@@ -11,6 +11,8 @@ controllaParametri(array('id','motivo','info'), 'presidente.utenti&errGen');
 $v = Volontario::id($_GET['id']);
 $attuale = $v->appartenenzaAttuale();
 $comitato = $attuale->comitato();
+$time = time();
+$motivo = "membro dimesso";
 
 /* Avviso il volontario */
 $m = new Email('dimissionevolontario', 'Dimissione Volontario: ' . $v->nomeCompleto());
@@ -28,7 +30,7 @@ $d->appartenenza    = $attuale;
 $d->comitato        = $comitato;
 $d->motivo = $_POST['motivo'];
 $d->info = $_POST['info'];
-$d->tConferma = time();
+$d->tConferma = $time;
 $d->pConferma = $me;
 
 /* Evitiamo di lasciare compiti a chi non è più in CRI */
@@ -71,52 +73,61 @@ $f = Reperibilita::filtra([
   ['volontario', $v]
   ]);
 foreach ($f as $_f) {
-    $_f->fine = time();
+    $_f->fine = $time;
 }
 
 $f = TitoloPersonale::filtra([
   ['volontario', $t]
   ]);
 foreach ($f as $_f) {
-    $_f->cancella();
+    $_f->fine = $time;
 }
-
-/* da rivedere da qui in poi */
-
-$f = Delegato::filtra([
-  ['volontario', $v]
-  ]);
-foreach ($f as $_f) {
-    $_f->fine = time();
-}
-
-$f = Partecipazione::filtra([
-  ['volontario', $v]
-  ]);
-foreach ($f as $_f) {
-    $_f->cancella();
-}
-
 
 $f = Estensione::filtra([
   ['volontario', $v]
   ]);
 foreach ($f as $_f) {
-    $_f->cancella();
+  if($_f->stato == EST_OK || $_f->stato == EST_AUTO){
+    $_f->termina();
+  }elseif($_f->stato == EST_INCORSO){
+    $_f->nega($motivo);
+  }
 }
 
 $f = Riserva::filtra([
   ['volontario', $v]
   ]);
 foreach ($f as $_f) {
-    $_f->cancella();
+  if($_f->stato == RISERVA_OK || $_f->stato == RISERVA_AUTO){
+    $_f->termina();
+  }elseif($_f->stato == RISERVA_INCORSO){
+    $_f->nega($motivo);
+  }
 }
 
 $f = Trasferimento::filtra([
   ['volontario', $v]
   ]);
 foreach ($f as $_f) {
-    $_f->cancella();
+  if($_f->stato == TRASF_INCORSO){
+    $_f->nega($motivo);
+  }
+}
+
+$p = Partecipazione::filtra([
+  ['volontario', $v]
+]);
+foreach ($p as $_p) {
+  if ( $_p->turno()->futuro() && $_p->turno()->attivita()->comitato() == $c->id) {
+    $_p->cancella();
+  }
+}
+
+$f = Delegato::filtra([
+  ['volontario', $v]
+  ]);
+foreach ($f as $_f) {
+  $_f->fine = $fine;
 }
 
 /* Chiudo l'appartenenza e declasso a persona */
