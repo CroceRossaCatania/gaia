5a6222806a2378694ddad17bfb3c2bb1d4f9f37c
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2014-01-07 17:56:36 +0000

    Fix #715, compressione GZIP per API

--- api.php
+++ api.php
@@ -6,8 +6,13 @@
 
 require('./core.inc.php');
 
+// Attiva la compressione GZIP
+ob_start('ob_gzhandler');
+
+// Imposta la risposta in JSON
 header('Content-type: application/json');
 
+// Crea la sessione API
 $api = new APIServer( @$_REQUEST['sid'] );
 $api->par = array_merge($_POST);
 echo $api->esegui($_GET['a']);
