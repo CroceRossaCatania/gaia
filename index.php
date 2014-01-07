e8d34493e4ea189c133bae7ce4aa4edd452d44a8
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2014-01-07 23:47:13 +0000

    Ref #821, pulsante admin API Keys

--- index.php
+++ index.php
@@ -269,6 +269,7 @@ $_descrizione   = 'Crediamo in una Croce Rossa Italiana che sa muoversi veloceme
                                     <li><a href="?p=admin.report"><i class="icon-copy"></i> Report</a></li>  
                                     <li><a href="?p=admin.format"><i class="icon-upload"></i> Carica format</a></li>                                    
                                     <li><a href="?p=admin.cache"><i class="icon-cloud"></i> Cache</a></li>  
+                                    <li><a href="?p=admin.chiavi"><i class="icon-code"></i> API Keys</a></li>  
                                 </ul>
                             </div>
                             <?php } ?>
