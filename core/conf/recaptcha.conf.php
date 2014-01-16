c712178d32c2b4354232495afb20c73f8ef72d7a
Federico <ico88@hotmail.com>
2014-01-16 22:06:44 +0100

    Fix #892 fatal error, rimosso doppio pulsante fix #890

---
+++ core/conf/recaptcha.conf.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * (c)2013 Croce Rossa Italiana
+ */
+
+/*
+ * Configurazione Recaptcha
+ * ============================
+ * 1. Andare su google.com/recaptcha ed ottenere una coppia di chiavi per il sito
+ * 2. Inserire qui la coppia di chiavi ottenute
+ * 3. Enjoy!
+ */
+
+$conf['recaptcha'] = [
+	'private_key'	=>	'6LfMEO0SAAAAAII0NSfUw2SyFTipsiOLYQYIRUy4 ',
+	'public_key'	=>	'6LfMEO0SAAAAAEdUMHv-TRLUeofr4ia0YzVUUIZH '
+];
