c3ce745407bbab41fba8f8b896e3cbec7b90dd1e
luca-dex <luca.desano@gmail.com>
2014-01-19 22:26:12 +0100

    tu, mortale, non inserire rinnovi casuali.

--- js/us.quote.nuova.js
+++ js/us.quote.nuova.js
@@ -1,6 +1,11 @@
 $(document).ready( function() {
  	var min = $("#inputData").attr('data-inizio');
     var max = $("#inputData").attr('data-fine');
+    var x = max.split('/');
+    var date = new Date(x[2], x[0], x[1]);
+    if (date > new Date()) {
+    	max = new Date();
+    }
     $("#inputData").datepicker({
     	minDate: min,
     	maxDate: max
