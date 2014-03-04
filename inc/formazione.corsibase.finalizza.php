d95ec4b85b7c7f54453e8b213fecc63d3d94d31f
luca-dex <luca.desano@gmail.com>
2014-03-04 22:49:26 +0100

    finito il JS :) #258

--- inc/formazione.corsibase.finalizza.php
+++ inc/formazione.corsibase.finalizza.php
@@ -61,14 +61,14 @@ $part = $corso->partecipazioni(ISCR_CONFERMATA);
                         </td>
                         <td>
                             <label class="radio">
-                                <input type="radio" name="p1_<?= $p->id; ?>" 
+                                <input type="radio" name="p1_<?= $p->id; ?>"  id="ct1_<?= $p->id; ?>"
                                 value="1" >
                                 Positivo
                             </label>
                         </td>
                         <td>
                             <label class="radio">
-                                <input type="radio" name="p1_<?= $p->id; ?>" 
+                                <input type="radio" name="p1_<?= $p->id; ?>" id="cf1_<?= $p->id; ?>"
                                 value="0" >
                                 Negativo
                             </label>
@@ -80,30 +80,30 @@ $part = $corso->partecipazioni(ISCR_CONFERMATA);
                         </td>
                         <td>
                             <label class="radio">
-                                <input class="p2_<?= $p->id; ?>" type="radio" name="p2_<?= $p->id; ?>" 
+                                <input class="p2_<?= $p->id; ?>" type="radio" name="p2_<?= $p->id; ?>" id="ct2_<?= $p->id; ?>"
                                 value="1" >
                                 Positivo
                             </label>
                         </td>
                         <td>
                             <label class="radio">
-                                <input class="p2_<?= $p->id; ?>" type="radio" name="p2_<?= $p->id; ?>" 
+                                <input class="p2_<?= $p->id; ?>" type="radio" name="p2_<?= $p->id; ?>" id="cf2_<?= $p->id; ?>"
                                 value="0" >
                                 Negativo
                             </label>
                         </td>
                     </tr>
                     <tr class="nascosto" id="opt_p3_<?= $p->id; ?>">
-                        <td colspan="2">
+                        <td colspan="2" id="tdex1_<?= $p->id; ?>">
                            <label class="checkbox inline">
-                                <input type="checkbox" id="extra_1_<?= $p->id; ?>" 
+                                <input type="checkbox" id="extra_1_<?= $p->id; ?>"
                                  name="extra_1_<?= $p->id; ?>" value="1"> 
                                  Prova pratica su Parte 2 sostituita da colloquio
                             </label>
                         </td>
                         <td colspan="2">
                             <label class="checkbox inline">
-                                <input type="checkbox" data-extra2="<?= $p->id; ?>" 
+                                <input type="checkbox" data-extra2="<?= $p->id; ?>" id="ex2_<?= $p->id; ?>"
                                  name="extra_2_<?= $p->id; ?>" value="1"> 
                                  Verifica effettuata solo sulla Parte 1 del programma del corso
                             </label>
--- js/formazione.corsibase.finalizza.js
+++ js/formazione.corsibase.finalizza.js
@@ -1,4 +1,7 @@
 $(document).ready( function() {
+
+  // Selezione blocco ammesso
+
   $("[data-ammesso]").each( function(i, e){
     $(e).click( function(){
       var id = $(e).data('ammesso');
@@ -12,6 +15,8 @@ $(document).ready( function() {
         }
     });
   });
+
+  // Selezione blocco non ammesso
 
   $("[data-non]").each( function(i, e){
     $(e).click( function(){
@@ -28,6 +33,8 @@ $(document).ready( function() {
     });
   });
 
+  // Selezione blocco assente
+
   $("[data-assente]").each( function(i, e){
     $(e).click( function(){
       var id = $(e).data('assente');
@@ -42,6 +49,9 @@ $(document).ready( function() {
         }
     });
   });
+
+// Se verifica solo su parte 1 disabilita blocco parte 2
+
   $("[data-extra2]").each( function(i, e){
     $(e).click( function(){
       var id = $(e).data('extra2');
@@ -49,9 +59,112 @@ $(document).ready( function() {
         $('.p2_' + id).attr('disabled', 'disabled');
         $('.p2_' + id).removeAttr('checked');
         $('#opt_p2_' + id).addClass('muted');
+        $('#tdex1_' + id).addClass('muted');
+        $('#extra_1_' + id).attr('disabled', true);
+        $('#extra_1_' + id).attr('checked', false);
+        // se hai anche superato parte 1
+        if($("#ct1_" + id).is(':checked')) {
+          $('#riga_' + id).removeClass('error');
+          $('#opt_p1_' + id).removeClass('error');
+          $('#opt_p2_' + id).removeClass('error');
+          $('#opt_p3_' + id).removeClass('error');
+          $('#tdex1_' + id).addClass('muted');
+          $('#extra_1_' + id).attr('disabled', true);
+          $('#extra_1_' + id).attr('checked', false);
+          $('#riga_' + id).addClass('success');
+          $('#opt_p1_' + id).addClass('success');
+          $('#opt_p2_' + id).addClass('success');
+          $('#opt_p3_' + id).addClass('success');
+        }
       } else {
         $('.p2_' + id).removeAttr('disabled');
         $('#opt_p2_' + id).removeClass('muted');
+        $('#riga_' + id).removeClass('success');
+        $('#opt_p1_' + id).removeClass('success');
+        $('#opt_p2_' + id).removeClass('success');
+        $('#opt_p3_' + id).removeClass('success');
+        $('#tdex1_' + id).removeClass('muted');
+        $('#extra_1_' + id).removeAttr('disabled');
+      }
+    });
+  });
+
+  // Se passato colora di verde altrimenti di rosso
+
+  $("[data-ammesso]").each( function(i, e){
+    var id = $(e).data('ammesso');
+
+    // Superata parte 1...
+
+    $("#ct1_" + id).click( function(){
+      // ...e parte 2
+      if($("#ct1_" + id).is(':checked') && $("#ct2_" + id).is(':checked')) {
+        $('#riga_' + id).removeClass('error');
+        $('#opt_p1_' + id).removeClass('error');
+        $('#opt_p2_' + id).removeClass('error');
+        $('#opt_p3_' + id).removeClass('error');
+        $('#riga_' + id).addClass('success');
+        $('#opt_p1_' + id).addClass('success');
+        $('#opt_p2_' + id).addClass('success');
+        $('#opt_p3_' + id).addClass('success');
+      }
+      // ... e verifica solo su parte 1
+      if($("#ct1_" + id).is(':checked') && $("#ex2_" + id).is(':checked')) {
+        $('#riga_' + id).removeClass('error');
+        $('#opt_p1_' + id).removeClass('error');
+        $('#opt_p2_' + id).removeClass('error');
+        $('#opt_p3_' + id).removeClass('error');
+        $('#tdex1_' + id).addClass('muted');
+        $('#riga_' + id).addClass('success');
+        $('#opt_p1_' + id).addClass('success');
+        $('#opt_p2_' + id).addClass('success');
+        $('#opt_p3_' + id).addClass('success');
+      }
+    });
+
+    // Parte uno non superata
+
+    $("#cf1_" + id).click( function(){
+      if($("#cf1_" + id).is(':checked')){
+        $('#riga_' + id).removeClass('success');
+        $('#opt_p1_' + id).removeClass('success');
+        $('#opt_p2_' + id).removeClass('success');
+        $('#opt_p3_' + id).removeClass('success');
+        $('#riga_' + id).addClass('error');
+        $('#opt_p1_' + id).addClass('error');
+        $('#opt_p2_' + id).addClass('error');
+        $('#opt_p3_' + id).addClass('error');
+      }
+    });
+
+    // Superata parte 2...
+
+    $("#ct2_" + id).click( function(){
+      // ...e parte 1
+      if($("#ct1_" + id).is(':checked') && $("#ct2_" + id).is(':checked')) {
+        $('#riga_' + id).removeClass('error');
+        $('#opt_p1_' + id).removeClass('error');
+        $('#opt_p2_' + id).removeClass('error');
+        $('#opt_p3_' + id).removeClass('error');
+        $('#riga_' + id).addClass('success');
+        $('#opt_p1_' + id).addClass('success');
+        $('#opt_p2_' + id).addClass('success');
+        $('#opt_p3_' + id).addClass('success');
+      }
+    });
+
+    // Parte 2 non superata
+
+    $("#cf2_" + id).click( function(){
+      if($("#cf2_" + id).is(':checked')){
+        $('#riga_' + id).removeClass('success');
+        $('#opt_p1_' + id).removeClass('success');
+        $('#opt_p2_' + id).removeClass('success');
+        $('#opt_p3_' + id).removeClass('success');
+        $('#riga_' + id).addClass('error');
+        $('#opt_p1_' + id).addClass('error');
+        $('#opt_p2_' + id).addClass('error');
+        $('#opt_p3_' + id).addClass('error');
       }
     });
   });       
