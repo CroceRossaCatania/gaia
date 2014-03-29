d263e1901d862d969f57e7f480b19ca982757e35
Alberto <ciopper90dev@gmail.com>
2014-03-30 00:11:05 +0100

    ora api dettagli ritorna anche info e dettagli referente fix #1125

--- core/class/APIServer.php
+++ core/class/APIServer.php
@@ -278,6 +278,10 @@ class APIServer {
         return [
             'nome'      =>  $a->nome,
             'comitato'  =>  $a->comitato()->toJSON(),
+	    'descrizione' => $a->descrizione,
+	    'referente' => $a->referente()->nome . ' ' . $a->referente()->cognome,
+	    'referentenum' => $a->referente()->cellulare(),
+	    'referenteemail' => $a->referente()->email(),
             'luogo'     =>  $a->luogo,
             'coordinate'=>  $a->coordinate(),
             'turni'     =>  $t
