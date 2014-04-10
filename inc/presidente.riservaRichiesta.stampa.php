1c12bfab53654fc5a5c354ea40d72a5b15f76d0e
luca-dex <luca.desano@gmail.com>
2014-04-10 18:38:27 +0200

    richiesta riserva a presidente #1191

--- inc/presidente.riservaRichiesta.stampa.php
+++ inc/presidente.riservaRichiesta.stampa.php
@@ -12,37 +12,42 @@ $f = $_GET['id'];
 $t = Riserva::id($f);
 
 $c = $t->volontario()->unComitato();
-$app = Appartenenza::filtra([
-    ['volontario',  $t->volontario()->id],
-    ['comitato',    $c->id]
-]);
-$app = $app[0];
+$app = $me->appartenenzaAttuale();
 
 /* Modificando questo, modificare anche utente.trasferimento.ok */
 $p = new PDF('riserva', 'Riserva.pdf');
-$p->_COMITATO = $c->locale()->nome;
-$p->_NOME = $t->volontario()->nome;
-$p->_COGNOME = $t->volontario()->cognome;
-$p->_LUOGO = $t->volontario()->comuneNascita;
-$p->_DATA = date('d/m/Y', $t->volontario()->dataNascita);
-$p->_ANNOCRI = date('d/m/Y', $app->inizio);
-$p->_MOTIVO = $t->motivo;
-$p->_INIZIO = date('d/m/Y', $t->inizio);
-$p->_FINE = date('d/m/Y', $t->fine);
-$p->_TIME = date('d/m/Y', $t->timestamp);
+$p->_COMITATO   = $c->locale()->nome;
+$p->_NOME       = $t->volontario()->nome;
+$p->_COGNOME    = $t->volontario()->cognome;
+$p->_LUOGO      = $t->volontario()->comuneNascita;
+$p->_DATA       = date('d/m/Y', $t->volontario()->dataNascita);
+$p->_ANNOCRI    = date('d/m/Y', $app->inizio);
+$p->_MOTIVO     = $t->motivo;
+$p->_INIZIO     = date('d/m/Y', $t->inizio);
+$p->_FINE       = date('d/m/Y', $t->fine);
+$p->_TIME       = date('d/m/Y', $t->timestamp);
 $f = $p->salvaFile();
 
 if ( $sessione->inGenerazioneRiserva) {
     $sessione->inGenerazioneRiserva = null;
     
     /* Richiesta all'utente */
-        $m = new Email('richiestaRiserva', 'Richiesta riserva');
-        $m->a = $me;
-        $m->_NOME       = $me->nome;
-        $m-> _TIME = date('d-m-Y', $t->timestamp);
-        $m->allega($f);
-        $m->invia();
-              
+    $m = new Email('richiestaRiserva', 'Richiesta riserva');
+    $m->a       = $me;
+    $m->_NOME   = $me->nome;
+    $m->_TIME   = date('d/m/Y', $t->timestamp);
+    $m->allega($f);
+    $m->accoda();
+
+    /* Richiesta all'utente */
+    $m = new Email('richiestaRiserva.presidente', 'Richiesta riserva da ' . $me->nomeCompleto);
+    $m->a       = $c->unPresidente();
+    $m->_NOME   = $me->nomeCompleto();
+    $m->_MOTIVO = $t->motivo;
+    $m->_TIME   = date('d/m/Y', $t->timestamp);
+    $m->allega($f);
+    $m->accoda();
+          
     redirect('utente.riserva&ok');
 } else {
     
