b32eda937b21aa3058bb41c020f4495f293a8391
luca-dex <luca.desano@gmail.com>
2014-04-19 16:19:12 +0200

    migliora cancellazione utente

--- inc/admin.utente.cancella.php
+++ inc/admin.utente.cancella.php
@@ -16,15 +16,50 @@ foreach($f as $_f){
     $_f->cancella();
 }
 
-$f = Appartenenza::filtra([
+$app = Appartenenza::filtra([
   ['volontario', $t]
   ]);
-if($f) {
-  $a = $t->appartenenzaAttuale();
+$a = $t->appartenenzaAttuale();
+
+if($a) {
   $c = $a->comitato();
   $a = $a->id;
 }
-foreach($f as $_f){
+
+
+// roba legata ad appartenenza attuale
+if($a) {
+  $f = Attivita::filtra([
+    ['referente', $t]
+    ]);
+  foreach($f as $_f){
+      $_f->referente = $c->unPresidente();
+  }
+
+  $f = Autorizzazione::filtra([
+    ['volontario', $t]
+    ]);
+  foreach($f as $_f){
+      $_f->volontario = $c->unPresidente();
+  }
+
+  if($c) {
+  $f = Gruppo::filtra([
+    ['referente', $t]
+    ]);
+  foreach($f as $_f){
+      $_f->referente = $c->unPresidente();
+  }
+}
+}
+
+
+// roba generica
+
+$f = Avatar::filtra([
+  ['utente', $t]
+  ]);
+foreach ($f as $_f) {
     $_f->cancella();
 }
 
@@ -33,27 +68,6 @@ $f = Area::filtra([
   ]);
 foreach($f as $_f){
     $_f->dimettiReferente();
-}
-
-$f = Attivita::filtra([
-  ['referente', $t]
-  ]);
-foreach($f as $_f){
-    $_f->referente = $c->unPresidente();
-}
-
-$f = Autorizzazione::filtra([
-  ['volontario', $t]
-  ]);
-foreach($f as $_f){
-    $_f->volontario = $c->unPresidente();
-}
-
-$f = Avatar::filtra([
-  ['utente', $t]
-  ]);
-foreach ($f as $_f) {
-    $_f->cancella();
 }
 
 $f = Commento::filtra([
@@ -105,15 +119,6 @@ foreach ($f as $_f) {
     $_f->cancella();
 }
 
-if($c) {
-  $f = Gruppo::filtra([
-    ['referente', $t]
-    ]);
-  foreach($f as $_f){
-      $_f->referente = $c->unPresidente();
-  }
-}
-
 $f = AppartenenzaGruppo::filtra([
   ['volontario', $t]
   ]);
@@ -133,15 +138,6 @@ $f = Privacy::filtra([
   ]);
 foreach ($f as $_f) {
     $_f->cancella();
-}
-
-if($a) {
-  $f = Quota::filtra([
-    ['appartenenza', $a]
-    ]);
-  foreach ($f as $_f) {
-      $_f->cancella();
-  }
 }
 
 $f = Reperibilita::filtra([
@@ -179,6 +175,23 @@ foreach ($f as $_f) {
     $_f->cancella();
 }
 
+// roba legata a tutte le appartenenza
+
+foreach($app as $_app) {
+  $f = Quota::filtra([
+    ['appartenenza', $_app]
+    ]);
+  foreach ($f as $_f) {
+      $_f->cancella();
+  }
+}
+
+// cancella appartenenza
+foreach($app as $_app){
+    $_app->cancella();
+}
+
+// cancella anagrafica
 $t->cancella();
 
 redirect('presidente.utenti&ok');    
