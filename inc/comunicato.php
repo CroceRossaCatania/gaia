d325f6038624f8f67d53192dceff483c1641c92f
luca-dex <luca.desano@gmail.com>
2014-06-11 19:08:34 +0200

    testo, modale e warning

---
+++ inc/comunicato.php
@@ -0,0 +1,129 @@
+<?php
+
+/*
+ * ©2014 Croce Rossa Italiana
+ */
+$sessione->deadline = time();
+paginaPrivata();
+
+?>
+
+<p>
+15 giugno 2014<br/>
+<h2>Nota della Squadra di Gaia</h2>
+<h3>Interruzione dello Sviluppo, del Supporto e dell'assistenza sul portale gaia.cri.it</h3>
+<p>
+
+<p>
+Ciao <?php echo $me->nome ?>,<br/>
+come avrai potuto notare, in queste ultime settimane il portale che ospita il Progetto Gaia è poco responsivo e la squadra di Supporto non è più in grado di fornire assistenza con la stessa assiduità con cui lo faceva prima.
+</p>
+<p>
+<strong>Cosa è successo?</strong><br/>
+Negli ultimi mesi un grandissimo numero di Comitati ha aderito al Progetto e di conseguenza il numero di volontari e soci registrati ha superato i <strong>56.000</strong>, facendo crescere drasticamente il carico di lavoro sull’infrastruttura informatica e rendendo <strong>improrogabile</strong> l’avvio di una serie di interventi e di migliorie per permettere al sistema di continuare a servire l’utenza.
+</p>
+
+<p>
+Purtroppo a questo incremento di utenti non è seguito <strong>nessuno stanziamento di fondi</strong> da parte della Croce Rossa per rendere disponibili a noi sviluppatori, e quindi a tutti gli utenti, le risorse che da ormai un anno stiamo chiedendo. 
+</p>
+
+<p>
+Nessuna delle numerose richieste che sono state avanzate per migliorare l’infrastruttura e gli strumenti tecnici necessari al Progetto si è concretizzata. Queste includevano ulteriori server, macchine più potenti, costi di licenze, strumenti di sviluppo. Noi sviluppatori siamo sempre stati abituati a lavorare in economia ed a sfruttare al meglio le risorse presenti, ma non e’ più pensabile che il Progetto possa servire tutta Italia con le stesse risorse con le quali è stati avviato.
+</p>
+
+Grafico delle pagine servite per giorno dal Progetto, attuali 32mila/giorno
+
+<p>
+Alla data attuale, nonostante le decine di richieste e l’impegno costante che abbiamo messo nel Progetto, <strong>nessun investimento è stato fatto da parte della CRI su di esso</strong>. Purtroppo le procedure burocratiche a cui dobbiamo tutti sottostare hanno fatto fallire ogni tentativo iniziato dal gruppo di lavoro dell’Area VI e dagli sviluppatori di Gaia. Gli unici soldi spesi per rendere funzionante Gaia sono stati spesi direttamente dai volontari che ci hanno lavorato ininterrottamente e dal personale dell’area VI, di tasca propria, per evitare che tutto si fermasse.
+</p>
+
+<p>
+<strong>Cosa ha fatto il team di Gaia?</strong><br/>
+Circa un mese fa, abbiamo comunicato definitivamente all’Area VI la nostra <strong>impossibilità nel continuare a sostenere una situazione di questo tipo</strong>, indicando la fine di giugno come termine ultimo per investire i fondi necessari oppure dare autorizzazione al Progetto di ricevere donazioni online per il suo stesso mantenimento o ancora, infine, permettere ad un Comitato CRI di sostenere i corsi di Gaia per un periodo limitato in attesa che si trovasse il modo di risolvere la situazione. Tristemente, <strong>nessuna risposta</strong> è ci è arrivata.
+</p>
+
+<p>
+Crediamo fermamente che sia necessario essere seri e responsabili nei confronti degli utenti del
+sistema e che se si vuole servire un servizio questo deve essere erogato con standard di qualità
+accettabili, che siano al passo coi tempi sia per quanto riguarda gli aspetti tecnici sia per quanto
+riguarda la tipologia di servizio fornito. Preferiamo, per rispetto nei confronti di tutti gli utenti
+nel caso non sia possibile lavorare in queste condizioni, non erogare nessun servizio.	
+</p>
+
+<p>
+<strong>Cosa succederà ora?</strong><br/>
+Alla fine di questo mese, nel caso in cui nulla cambierà, non svilupperemo più nuove funzionalità, non rilasceremo aggiornamenti al sistema e non saremo più in grado di dare seguito alle richieste di assistenza che arriveranno tramite i canali di supporto.
+</p>
+
+<p>
+<strong>Cosa devo fare?</strong><br/>
+Se sei un presidente o un delegato ufficio soci, ti invitiamo a scaricare e conservare gli elenchi dei volontari e dei soci ordinari che hai caricato su Gaia, dalle apposite sezioni, di modo da non perdere i dati.
+</p>
+
+<p>
+<strong>Nuove funzionalità che non vedranno la luce</strong><br/>
+Moltissime funzionalità di Gaia in questi mesi sono state sviluppate partendo dai regolamenti e dalle esigenze dei comitati CRI. Alcune di queste, ormai pronte per essere rilasciate, non verranno messe a disposizione in quanto non potranno essere adeguatamente mantenute.</p>
+<p>
+Le funzionalità sviluppate e non presenti sul sistema sono le seguenti:
+</p>
+<p>
+<ul>
+	<li>Sistema di gestione dei corsi base (completo)</li>
+	<li>Sistema di gestione degli aspiranti volontari con la funzionalità "Cerca il corso base più
+	vicino a te" (completo)</li>
+	<li>Sistema di emissione, verifica e gestione dei tesserini per volontari e soci ordinari (colmpleto)</li>
+	<li>Sistema di gestione delle donazioni del sangue sviluppato dal gruppo di lavoro 
+	sulle donazioni (compelto)</li>
+	<li>Adeguamento del portale alle linee guida di comunicazione istituzionale (completo)</li>
+	<li>Nuovo sistema di gestione delle quote dei volontari che rispetta le indicazioni
+	legate alla provatizzazione dei comitati CRI(completo)</li>
+	<li>Introduzione di una timeline con le ultime informazioni sul mondo CRI (completo)</li>
+	<li>Gestione da parte dell'ufficio soci di trasferimenti, estensioni e riserve (completo)</li>
+	<li>Revisione del sistema di gestione degli elenchi, degli export e delle email (completo)</li>
+	<li>Introduzione del sistema di gestione dei provvedimenti disciplinari e 
+	delle riammissioni secondo le ultime direttive (completo) </li>
+</ul>
+</p>
+
+<p>
+Oltre a quanto riportato in elenco è pronta l'applicazione ufficiale per smartphone Android e era
+in fase di completamento quella per smartphone iOS. Anche questi ulteriori strumenti purtroppo non
+vedranno la luce.
+</p>
+
+<p>
+<strong>E tanto altro ancora...</strong><br/>
+Tante sono poi le funzionalità di Gaia in fase di sviluppo, a cominciare dal sistema di gestione
+unificato per la formazione interna ed esterna all'associazione, passando per il modulo di 
+gestione delle visite mediche e degli aspetti sanitari che avrebbe integrato tutte le procedure
+di sicurezza per la gestione dei dati sensibili, arrivando al modulo per la gestione di vestiario
+e magazzini interni al comitato e tanto altro ancora.
+</p>
+
+<p>
+<strong>Ringraziamenti</strong><br/>
+Vorremmo ringrazia tutti gli utenti di Gaia che in questi mesi hanno contribuito a segnalarci bug e malfunzionamenti di ogni genere via email, telefono, github, facebook e con ogni altro strumento. Vorremmo inoltre ringraziare tutte le persone che ci hanno suggerito funzionalità e miglioramenti da introdurre nel sistema. Vorremmo ringraziare inoltre tutti i presidenti e gli uffici soci dei comitati che hanno aderito al progetto e hanno avuto la pazienza di attendere le risposte del supporto quando avevano qualche problema, di compilare in maniera corretta gli i formati e di spiegare ai loro volontari come utilizzare il sistema. Da ultimo ci teniamo a ringraziare tutto lo staff dell'helpdesk di primo
+livello che in questi mesi ci ha aiutato a smaltire il numero sempre maggiore di ticket da parte degli
+utenti.
+</p>
+
+<p>
+La nostra speranza è quella che la Croce Rossa Italiana decida, visti anche gli ottimi risultati raggiunti in poco tempo e con un budget nullo, di investire e far crescere questo progetto ma, al momento attuale, la strada sembra essere troppo in salita anche per noi.
+</p>
+
+<p>
+Firmato <br/>
+Gli Sviluppatori e la Squadra di Supporto di Gaia
+</p>
+
+<p>
+Alberto Copelli<br/>
+Luca De Sano<br/>
+Federico D’Urso<br/>
+Alfio Emanuele Fresta<br/>
+Paolo Giustiniani<br/>
+Stefano Principato<br/>
+Biagio Saitta<br/>
+Tommaso Scquizzato<br/>
+Giuseppe Titolo<br/>
+</p>
