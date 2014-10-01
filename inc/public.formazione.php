d30975033dd94e6d99f6a262311d88515b377d46
Alfio Emanuele <alfio.emanuele.f@gmail.com>
2014-10-01 15:24:15 +0100

    Pagina formazione su Drive, fix #1152

---
+++ inc/public.formazione.php
@@ -0,0 +1,5 @@
+<div class="row">
+
+	<iframe class="span12" style="border: none; min-height: 1800px;" src="https://docs.google.com/document/d/1tRrCVuGJRrMRVQhbMw0-X6s9TeEzvoOqz5zzGm4QgXQ/pub?embedded=true"></iframe>
+
+</div>
