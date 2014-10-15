9431516e116896575cbded7a08857cefb36774d4
Federico <ico88@hotmail.com>
2014-10-15 22:28:46 +0200

    Virgola dannata

--- inc/part/utente.menu.php
+++ inc/part/utente.menu.php
@@ -102,7 +102,7 @@ if ($me && $me->unComitato()) {
             'utente.estensione'  =>  '<i class="icon-random"></i> Estensioni',
             'utente.trasferimento'  =>  '<i class="icon-arrow-right"></i> Trasferimenti',
             'utente.riserva'  =>  '<i class="icon-pause"></i> Riserva',
-            'utente.rubricaReferenti'  =>  '<i class="icon-book"></i> Rubrica'
+            'utente.rubricaReferenti'  =>  '<i class="icon-book"></i> Rubrica',
             'public.tesserino'  =>  '<i class="icon-credit-card"></i> Verifica Tesserino'  
         ]];
 }
