80a431d4cb572f56dc2a784a845fa54f6ba625a2
Federico <ico88@hotmail.com>
2014-10-15 21:07:24 +0200

    Ref #1449, primo bug
    Mancavano costanti

--- core/libs/costanti.php
+++ core/libs/costanti.php
@@ -107,6 +107,20 @@ define('ATTIVITA_PUBBLICA',		1);
 
 /*
  * ===================================
+ * ========STATO ATTIVITA ============
+ * ===================================
+ */
+
+define('ATTIVITA_CHIUSA',      0);
+define('ATTIVITA_APERTA',     10);
+
+$conf['attivita_stato'] = [
+    ATT_CHIUSA     =>  'Attività chiusa',
+    ATT_APERTA     =>  'Attività attiva'
+];
+
+/*
+ * ===================================
  * =========== AUTORIZZAZIONE ========
  * ===================================
  */
