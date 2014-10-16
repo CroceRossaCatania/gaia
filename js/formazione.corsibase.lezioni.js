6edee8510e007a5b6bac45fff7482fc6f084a80d
luca-dex <luca.desano@gmail.com>
2014-10-16 22:20:50 +0100

    Fix #1472
    sistemare un po' le date

--- js/formazione.corsibase.lezioni.js
+++ js/formazione.corsibase.lezioni.js
@@ -1,13 +1,22 @@
 $(document).ready ( function() {
     
     $(".dti").datetimepicker({
-    	timeText: 'Alle:',
-    	hourText: 'Ore',
-    	minuteText: 'Minuti',
-    	currentText: 'Ora',
-    	closeText: 'Ok',
+        timeText: 'Alle:',
+        hourText: 'Ore',
+        minuteText: 'Minuti',
+        currentText: 'Ora',
+        closeText: 'Ok',
         defaultTimezone: '+0100',
         minDate: new Date()
+    });
+
+    $(".dtia").datetimepicker({
+        timeText: 'Alle:',
+        hourText: 'Ore',
+        minuteText: 'Minuti',
+        currentText: 'Ora',
+        closeText: 'Ok',
+        defaultTimezone: '+0100'
     });
 
     $(".modificabile input").change(function() {
