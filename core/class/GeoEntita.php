5cc9d40a441ab5f305c1654c458f980e5683017c
luca-dex <luca.desano@gmail.com>
2014-10-18 00:34:49 +0100

    porco cazzo

--- core/class/GeoEntita.php
+++ core/class/GeoEntita.php
@@ -25,7 +25,6 @@ abstract class GeoEntita extends Entita {
         $q->bindParam(':id', $this->id);
         $q->execute();
         if ( $r = $q->fetch(PDO::FETCH_NUM) ) {
-            var_dump($r);
             return [$r[0], $r[1]];
         } else {
             return [false, false];
@@ -196,9 +195,6 @@ abstract class GeoEntita extends Entita {
     public function linkMappa() {
         $n = urlencode($this->luogo);
         $c = $this->coordinate();
-        var_dump($this->coordinate());
-        var_dump($this->luogo);
-        echo("c: {$n}");
         $c = $c[0] . ',' . $c[1];
         return "http://maps.google.com/?q={$n}@{$c}";
     }
