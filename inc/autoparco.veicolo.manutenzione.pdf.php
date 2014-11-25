cd5b155b9a19db4624ea60e93f467418f3682463
Angelo Lupo <angelolupo94@gmail.com>
2014-11-25 19:50:59 +0000

    pagine creazione pdf manutenzione/rifornimento

--- inc/autoparco.veicolo.manutenzione.pdf.php
+++ inc/autoparco.veicolo.manutenzione.pdf.php
@@ -12,3 +12,16 @@ $veicolo = $manutenzione->veicolo();
 
 proteggiVeicoli($veicolo, [APP_AUTOPARCO, APP_PRESIDENTE]);
 
+$p = new PDF("manutenzioni","manutenzione{$_GET['id']}.pdf");
+
+$p->_TITOLO = "Manutenzione {$_GET['id']}";
+$p->_TIPO = $conf['man_tipo'][$manutenzione->tipo];
+$p->_KM = $manutenzione->km;
+$p->_DATA = date('d/m/Y', $manutenzione->tIntervento);
+$p->_DESCRIZIONE = $manutenzione->intervento;
+$p->_AZIENDA = $manutenzione->azienda();
+$p->_NOFAT = $manutenzione->fattura();
+$p->_COSTO = $manutenzione->costo;
+
+$f = $p->salvaFile();
+$f->download();
