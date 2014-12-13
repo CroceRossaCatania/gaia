2811bc696900bb3f9d64aa86e6b9e45966ac50c6
AlfioEmanueleFresta <alfio.emanuele.f@gmail.com>
2014-12-13 23:30:25 +0000

    Pagina dati tesserini

---
+++ inc/admin.jump.dati.step1.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * (c)2014 Croce Rossa Italiana
+ */
+
+paginaAdmin();
+
+caricaSelettore();
+
+$r = [];
+
+?>
+
+
+<table class="table table-condensed">
+	<tbody>
+		<?php
+		foreach ( $_POST['volontari'] as $codice ) { 
+			$codice = maiuscolo($codice);
+			if ( !$codice ) { continue; }
+			$v = Utente::daCodicePubblico($codice);
+			?>
+			<tr>
+				<td style="font-family: monospace;"><?= $codice; ?></td>
+				<td><?= $v->cognome; ?></td>
+				<td><?= $v->nome; ?></td>
+				<td style="font-family: monospace;"><?= $v->codiceFiscale; ?></td>
+			</tr>
+		<?php } ?>
+	</tbody>
+</table>
+
