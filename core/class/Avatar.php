82f5bee1ea5f2629e79c109d8b771829fb808a09
AlfioEmanueleFresta <alfio.emanuele.f@gmail.com>
2014-12-17 05:37:01 -0500

    Fix #1707, evita caching in browser nel cambio avatar e tesserini

--- core/class/Avatar.php
+++ core/class/Avatar.php
@@ -63,12 +63,13 @@ class Avatar extends Entita {
     /**
      * Ritorna sempre una immagine all'avatar o placeholder
      */
-    public function img( $dimensione ) {
-        if ( is_readable( $this->file($dimensione) ) ) {
-            return $this->file($dimensione);
-        } else {
+    public function img( $dimensione, $nocache = true ) {
+        if ( is_readable( $this->file($dimensione) ) )
+            return $this->file($dimensione)
+                    . File::noCacheQueryString($nocache);
+        else
             return "./upload/avatar/placeholder/$dimensione.jpg";
-        }
+        
     }
 
     /**
