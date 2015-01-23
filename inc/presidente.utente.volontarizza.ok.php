e180dd8bcbb7a7f845138a8e4684461489416f3e
Federico <ico88@hotmail.com>
2015-01-23 19:41:29 +0100

    Fix #1744,Volontarizza per us e presidente

---
+++ inc/presidente.utente.volontarizza.ok.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * ©2014 Croce Rossa Italiana
+ */
+
+paginaApp([APP_SOCI, APP_PRESIDENTE]);
+controllaParametri(['id']);
+
+$id = $_GET['id']; 
+$u = Utente::id($id);
+proteggiDatiSensibili($u);
+
+if($u->stato != PERSONA) {
+    redirect('errore.permessi&cattivo');
+}
+
+$app = $u->appartenenzaAttuale();
+$comitato = $app->comitato;
+
+if ( $_POST['dataInizio'] ) {
+    $inizio = @DateTime::createFromFormat('d/m/Y', $_POST['dataInizio']);
+    if ( $inizio ) {
+        $inizio = @$inizio->getTimestamp();
+        $app->fine = $inizio;
+    } else {
+        $app->fine = 0;
+    }
+}
+
+$u->stato = VOLONTARIO;
+$nuovaApp = new Appartenenza();
+$nuovaApp->volontario = $u;
+$nuovaApp->comitato = $comitato;
+$nuovaApp->stato = MEMBRO_VOLONTARIO;
+$nuovaApp->inizio = $inizio;
+$nuovaApp->timestamp = time();
+$nuovaApp->comferma = $me;
+
+redirect("presidente.utente.visualizza&innalzato&id={$id}");
