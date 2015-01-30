bfd697b9e028eea8ae72abe6b3183457da17e1cd
Federico D'Urso <ico88@hotmail.com>
2015-01-30 23:47:41 +0100

    Correzione entità

--- core/class/DonazioneMerito.php
+++ core/class/DonazioneMerito.php
@@ -10,7 +10,9 @@ class DonazioneMerito extends Entita {
     public static
         $_t     = 'donazioni_meriti',
         $_dt    = null;
-
+        
+    use EntitaCache;
+    
     public function volontario() {
         return Volontario::id($this->volontario);
     }
