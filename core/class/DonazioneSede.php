422fce8de3a87f9f29f80adc425c7b9f31f2886a
Michael Scarabelli <michael.scarabelli@etesya.com>
2015-02-12 11:11:35 +0100

    Modifiche selezione sede donazione

--- core/class/DonazioneSede.php
+++ core/class/DonazioneSede.php
@@ -24,7 +24,7 @@ class DonazioneSede extends Entita {
             $_where = static::preparaCondizioni($_where, 'WHERE');
         }
 
-        $query = "SELECT * FROM ( SELECT $_dettaglio, id FROM ". static::$_t . " $_where ORDER BY $_dettaglio ) AS tmp_table GROUP BY LOWER($_dettaglio)";
+        $query = "SELECT * FROM ( SELECT $_dettaglio, id FROM ". static::$_t . " $_where ORDER BY $_dettaglio DESC ) AS t GROUP BY $_dettaglio";
         
         /*
          * Controlla se la query è già in cache
