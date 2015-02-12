c513443f1c69ca62a09615e4c3c1ff643b2ea4d9
Michael Scarabelli <michael.scarabelli@etesya.com>
2015-02-12 14:05:19 +0100

    Aggiunta anagrafica donatore

--- inc/utente.donazione.anagrafica.ok.php
+++ inc/utente.donazione.anagrafica.ok.php
@@ -11,8 +11,11 @@ controllaParametri(array('inputSangueGruppo'));
 $p = new DonazioneAnagrafica();
 $p->volontario  = $me->id;
 $p->sangue_gruppo   = normalizzaNome($_POST['sede']);
-$p->fattore_rh = $_POST['inputFattoreRH'] ? normalizzaNome($_POST['inputFattoreRH']) : null;
-$p->fanotipo_rh = $_POST['inputFenotipoRH'] ? normalizzaNome($_POST['inputFenotipoRH']) : null;
-$p->kell = $_POST['inputKell'] ? normalizzaNome($_POST['inputKell']) : null;
+$inputFattoreRH = $_POST['inputFattoreRH'] ? normalizzaNome($_POST['inputFattoreRH']) : null;
+$p->__set('fattore_rh',$inputFattoreRH);
+$inputFenotipoRH = $_POST['inputFenotipoRH'] ? normalizzaNome($_POST['inputFenotipoRH']) : null;
+$p->__set('fanotipo_rh',$inputFenotipoRH);
+$kell = $_POST['inputKell'] ? normalizzaNome($_POST['inputKell']) : null;
+$p->__set('kell',$kell);
 
 redirect('utente.donazioni&ok&d=' . $t->tipo);
