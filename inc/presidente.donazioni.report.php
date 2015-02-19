194d1411e30f2600191bd37ec99127e669cd31aa
Michael Scarabelli <michael.scarabelli@etesya.com>
2015-02-19 11:35:31 +0100

    Presidente reportistaca donazioni

---
+++ inc/presidente.donazioni.report.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * ©2013 Croce Rossa Italiana
+ */
+
+paginaApp([APP_SOCI , APP_PRESIDENTE]);
+
+?>
+
+<br/>
+<div class="row-fluid">
+    <div class="span8">
+        <h2>
+            <i class="icon-copy muted"></i>
+            Report donazioni
+        </h2>
+    </div>   
+</div>
+
+
+        <table class="table table-striped table-bordered" id="tabellaUtenti">
+            <thead>
+                <th>Regione</th>
+                <th>Provincia</th>
+                <th>Città</th>
+				<th>Ospedale</th>
+                <th>Sangue Intero</th>
+				<th>Plasma</th>
+				<th>Multicomponenti</th>
+            </thead>
+            <?php 
+			foreach(DonazioneSedi::filtra() as $sede){
+				?>
+				<tr>
+                    <td><?php echo $sede[0]->regione; ?></td>
+                    <td><?php echo $sede[0]->provincia; ?></td>
+                    <td><?php echo $sede[0]->citta; ?></td>
+                    <td><?php echo $sede[0]->nome; ?></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+				</tr>
+				<?php
+			}
+			?>
+            </table>
