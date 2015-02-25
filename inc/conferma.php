b0b6c206aed4136805fa1e21573f2033b9322d9a
Paolo Giustiniani <paolo.giustiniani@cri.it>
2015-02-25 21:06:07 +0100

    Update conferma.php

--- inc/conferma.php
+++ inc/conferma.php
@@ -11,19 +11,19 @@ controllaBrowser();
 
 <div class="modal fade automodal">
     <div class="modal-header">
-        <h3 class="text-success"><i class="icon-user"></i> Sono già un volontario?!</h3>
+        <h3 class="text-success"><i class="icon-user"></i> Sono già un volontario!</h3>
     </div>
     <div class="modal-body">
         <p class="allinea-centro">
             <a href="?p=riconoscimento&tipo=volontario" class="btn btn-large btn-success">
                 <i class="icon-user"></i>
-                <strong>Confermo!</strong> <em>sono già un volontario!</em>
+                <strong>Confermo!</strong> <em>Sono già un volontario in Croce Rossa.</em>
             </a>
             <br/>
             <br/>
             <a href="?p=riconoscimento&tipo=aspirante" class="btn btn-primary btn-large">
                 <i class="icon-user"></i>
-                <strong>No!</strong> <em>vorrei diventare un volontario!</em>
+                <strong>No!</strong> <em>Vorrei diventare un volontario.</em>
             </a>
         </p>
     </div>
