175aa2851525858a53df57b8a1b8095a7c47ec21
dan-it <dan-it@users.noreply.github.com>
2015-04-29 11:38:19 +0200

    Spostato inserimento numero di tel in class PDF

--- inc/us.ordinario.nuovo.ok.php
+++ inc/us.ordinario.nuovo.ok.php
@@ -142,8 +142,6 @@ if ($importo >= $quotaBen) {
 /* Crea la ricevuta del pagamento della quota */
 $l = new PDF('ricevutaquota', 'ricevuta.pdf');
 $l->_COMITATO 	= $a->comitato()->locale()->nomeCompleto();
-$l->_INDIRIZZO 	= $a->comitato()->locale()->formattato;
-$l->_TEL 		= $a->comitato()->locale()->telefono;
 $l->_ID 		= $q->progressivo();
 $l->_NOME 		= $p->nome;
 $l->_COGNOME 	= $p->cognome;
