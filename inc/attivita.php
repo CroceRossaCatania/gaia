1da0240bf92088e1d1223b92d29b36a0476340dc
Pietro Ferraresi <pietro.ferraresi@gmail.com>
2015-05-19 22:42:00 +0200

    Modifica al layout e prove di integrazione nuova versione di full calendar andate male

--- inc/attivita.php
+++ inc/attivita.php
@@ -12,6 +12,12 @@ if ( $me instanceof Anonimo || $me->stato == ASPIRANTE ) {
 }
 
 ?>
+
+<script src="http://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.3.1/fullcalendar.min.js"></script>
+<link href='http://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.3.1/fullcalendar.min.css' rel='stylesheet' />
+<link href='http://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.3.1/fullcalendar.print.css' rel='stylesheet' />
+ 
+
 <div class="row-fluid">
         <div class="span3">
             <?php menuVolontario(); ?>
