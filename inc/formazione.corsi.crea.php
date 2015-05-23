c2e3ce016fa1d30a673f9a929a1ea08f2707db38
Pietro Ferraresi <pietro.ferraresi@gmail.com>
2015-05-23 18:13:05 +0200

    aggiornamenti form di creazione corso, uniformati gli stili

--- inc/formazione.corsi.crea.php
+++ inc/formazione.corsi.crea.php
@@ -1,127 +1,129 @@
 <?php
-
 /*
  * ©2014 Croce Rossa Italiana
  */
-
 $_titolo = "Calencario dei Corsi";
-
 ?>
-<style>
-/* Dello stile veloce...*/
-#calendar {
-        max-width: 900px;
-        margin: 0 auto;
-}
-</style>
-<link href='/assets/css/fullcalendar.min.2.3.1.css' rel='stylesheet' />
-<link href='/assets/css/fullcalendar.print.css' rel='stylesheet' media='print' />
-<script src='/assets/js/libs/moment.min.js'></script>
-<script src='/assets/js/fullcalendar.min.2.3.1.js'></script>
-<script src='/assets/js/lang/it.js'></script>
 
-<div class="row-fluid">
-    
+<div class="row-fluid" style="margin-top: 50px">
+
     <div class="span8">
-       
-        <form>
-            <article>
-                <h1 class="pagetitle">Scheda corso BLSD cat. A BARI </h1>
-                <div id="contentbody">
-
-                    in versione form
-                    <div class="calendarevent">
-                        <p class="eventlist"><strong>Sede:</strong><input type="text" value="BARI" /></p>										
-                        <p class="eventlist"><strong>Provincia:</strong>BA</p>										
-                        <p class="eventlist"><strong>Data inizio:</strong> 01 maggio 2015</p>										
-                        <p class="eventlist"><strong>Data fine:</strong> 01 maggio 2015</p>										
-                        <p class="eventlist"><strong>Direttore:</strong>EUGENIO PADALINO</p>																				<p class="eventlist">&nbsp;</p>
-                        <p class="eventlist"><strong>Per informazioni</strong></p>
-                        <p class="eventlist"><strong>Telefono:</strong>3937314505</p>																															<p class="eventlist"><strong>Corso aperto al pubblico:</strong> sì																				</p></div>
-                    <h2>istruttori</h2>
-                    <ul>
-                        <li>VITO MASTRODONATO</li>
-                    </ul>
-                    <div class="info-corso">Calendario dei corsi organizzati dalla <strong>Rete Formativa IRC</strong>. La scheda di ogni corso riporta i riferimenti dell'organizzatore locale (se assenti il corso non è aperto al pubblico) da contattare per tutte le informazioni.</div>
-                    <a href="?p=formazione.corsi.crea.ok" type="submit">crea</a>
-            </article>
+        <h2><i class="icon-plus-square icon-calendar muted"></i> Competenze personali</h2>
+        <form action="?p=utente.titolo.nuovo" method="POST">
+            <input value="1" name="idTitolo" id="idTitolo" type="hidden">
+            <div class="alert alert-block alert-success">
+                <div class="row-fluid">
+                    <h4><i class="icon-question-sign"></i> Dati del corso...</h4>
+                </div>
+                <hr>
+                <div class="row-fluid">
+                    <div class="span4">
+                        <label for="tipologia"><i class="icon-certificate"></i> Tipologia</label>
+                    </div>
+                    <div class="span8">
+                        <select>
+                            <option>tipologia</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row-fluid">
+                    <div class="span4">
+                        <label for="tipologia"><i class="icon-building"></i> Sede</label>
+                    </div>
+                    <div class="span8">
+                        <select>
+                            <option>tipologia</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row-fluid">
+                    <div class="span4">
+                        <label for="dataInizio"><i class="icon-calendar"></i> Data Di inizio</label>
+                    </div>
+                    <div class="span8">
+                        <input id="dataInizio" class="span12 hasDatepicker" name="dataInizio" value="" type="text">
+                    </div>
+                </div>
+                <div class="row-fluid">
+                    <div class="span4">
+                        <label for="dataFine"><i class="icon-calendar"></i> Data Di Fine</label>
+                    </div>
+                    <div class="span8">
+                        <input id="dataFine" class="span12 hasDatepicker" name="dataFine" value="" type="text">
+                    </div>
+                </div>
+                <div class="row-fluid">
+                    <div class="span4">
+                        <label for="dataFine"><i class="icon-user-md"></i> Direttore</label>
+                    </div>
+                    <div class="span8">
+                        <select>
+                            <option>tipologia</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row-fluid">
+                    <div class="span4">
+                        <label for="dataFine"><i class="icon-user"></i> Istruttori</label>
+                    </div>
+                    <div class="span8">
+                        <select class="chosen-select">
+                            <option>tipologia</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row-fluid">
+                    <div class="span4">
+                        <label for="dataFine"><i class="icon-user"></i> Istruttori aiuto</label>
+                    </div>
+                    <div class="span8">
+                        <select class="chosen-select">
+                            <option>tipologia</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row-fluid">
+                    <div class="span4 offset8">
+                        <button type="submit" class="btn btn-success">
+                            <i class="icon-plus"></i>
+                            Aggiungi il titolo
+                        </button>
+                    </div>
+                </div>
+            </div>
         </form>
-        <hr/>
-        
-        <section>
-            <h4>Iscritti</h4>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Nome</th>
-                        <th>Cognome</th>
-                        <th>Email</th>
-                        <th>Stato</th>
-                        <th>Azioni</th>
-                    </tr>
-                </thead>
-                <tbody>
-                     <tr>
-                        <td>Id</td>
-                        <td>Donald</td>
-                        <td>Duck</td>
-                        <td>d.duck@email.com</td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Id</td>
-                        <td>Daisy</td>
-                        <td>Duck</td>
-                        <td>da.duck@email.com</td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Id</td>
-                        <td>Michele</td>
-                        <td>Mouse</td>
-                        <td>m.mouse@email.com</td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-            
-            invita
-            <input type="" />
-            
-       </section>
-        
     </div>
-    
+
     <div class="span4">
-         <h4>Se hai i permessi</h4>
+        <h4 style="line-height:40px">Azioni</h4>
         <nav>
-            permessi corso: pubblico | privato
-            
-            <a class="btn btn-danger" href="?p=utente.me">
-                <i class="icon-plus-sign-alt icon-large"></i>&nbsp;
-                Eliminare il corso --> annulla
-            </a>
-            <br/>
-            <a class="btn btn-danger" href="?p=utente.me">
-                <i class="icon-plus-sign-alt icon-large"></i>&nbsp;
-                Convalidare
-            </a>
-            <br/>
-            <a class="btn btn-danger" href="?p=utente.me">
-                <i class="icon-plus-sign-alt icon-large"></i>&nbsp;
-                richiedi iscrizione
-            </a>
-            <br/>
-            <a class="btn btn-danger" href="?p=utente.me">
-                <i class="icon-plus-sign-alt icon-large"></i>&nbsp;
-                chiudi corso -> valutazione
-            </a>
+            <ul style="list-style-type: none">
+                <li>
+                    <a class="btn btn-danger" href="?p=utente.me">
+                        <i class="icon-plus-sign-alt icon-large"></i>&nbsp;
+                        Eliminare il corso --> annulla
+                    </a>
+                </li>
+                <li>
+                    <a class="btn btn-danger" href="?p=utente.me">
+                        <i class="icon-plus-sign-alt icon-large"></i>&nbsp;
+                        Convalidare
+                    </a>
+                </li>
+                <li>
+                    <a class="btn btn-danger" href="?p=utente.me">
+                        <i class="icon-plus-sign-alt icon-large"></i>&nbsp;
+                        richiedi iscrizione
+                    </a>
+                </li>
+                <li>
+                    <a class="btn btn-danger" href="?p=utente.me">
+                        <i class="icon-plus-sign-alt icon-large"></i>&nbsp;
+                        chiudi corso -> valutazione
+                    </a>
+                </li>
+            </ul>
         </nav>
-        
+
     </div>
-        
 </div>
