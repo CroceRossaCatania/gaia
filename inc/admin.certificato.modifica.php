39775d03757f5f3aedf6c57619e4c6c0f4cb8fa7
developer <developer@gaia-vdi>
2015-07-10 23:23:59 -1000

    Modifica al form di inserimento di un tipo di corso e modifica alla tabella associata

--- inc/admin.certificato.modifica.php
+++ inc/admin.certificato.modifica.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * ©2013 Croce Rossa Italiana
  */
@@ -22,19 +21,93 @@ $f = Certificato::id($t);
 <form class="form-horizontal" action="?p=admin.certificato.modifica.ok&id=<?php echo $t; ?>" method="POST">
 
     <div class="control-group">
-      <label class="control-label" for="inputNome">Nome certificato</label>
-      <div class="controls">
-        <input class="input-xxlarge" type="text" name="inputNome" id="inputNome" value="<?php echo $f->nome; ?>">
-        
+        <label class="control-label" for="inputNome">Nome certificato</label>
+        <div class="controls">
+            <input class="input-xxlarge" type="text" name="inputNome" id="inputNome" value="<?php echo $f->nome; ?>">
+        </div>
     </div>
-</div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputNome">Numero discenti</label>
+        <div class="controls">
+            minimo:     <input class="input-small" type="text" name="inputDurata" id="inputDurata" value="<?php echo $f->durata; ?>">
+            &nbsp;massimo:    <input class="input-small" type="text" name="inputDurata" id="inputDurata" value="<?php echo $f->durata; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputNome">Durata</label>
+        <div class="controls">
+            ore:     <input class="input-small" type="text" name="inputDurata" id="inputDurata" value="<?php echo $f->durata; ?>">
+            &nbsp;giorni:    <input class="input-small" type="text" name="inputDurata" id="inputDurata" value="<?php echo $f->durata; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputNome">Limite in giorni per l'iscrizione</label>
+        <div class="controls">
+            <input class="input-small" type="text" name="inputDurata" id="inputDurata" value="<?php echo $f->durata; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputNome">Attestato</label>
+        <div class="controls">
+            <input class="input-xxlarge" type="text" name="inputRapporto" id="inputRapporto" value="<?php echo $f->rapporto; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputNome">Tipo Valutazione</label>
+        <div class="controls">
+            <input class="input-xxlarge" type="text" name="inputRapporto" id="inputRapporto" value="<?php echo $f->rapporto; ?>">
+        </div>
+    </div>
+    
+    <hr/>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputNome">Ruolo Proprietario</label>
+        <div class="controls">
+            <input class="input-xxlarge" type="text" name="inputRapporto" id="inputRapporto" value="<?php echo $f->rapporto; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputNome">Ruolo Direttore</label>
+        <div class="controls">
+            <input class="input-xxlarge" type="text" name="inputRapporto" id="inputRapporto" value="<?php echo $f->rapporto; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputNome">Ruolo Docente</label>
+        <div class="controls">
+            <input class="input-xxlarge" type="text" name="inputRapporto" id="inputRapporto" value="<?php echo $f->rapporto; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputNome">Ruolo Docente di Affiancamento</label>
+        <div class="controls">
+            <input class="input-large" type="text" name="inputRapporto" id="inputRapporto" value="<?php echo $f->rapporto; ?>">
+            &nbsp;Proporzione: 1 / <input class="input-small" type="text" name="inputDurata" id="inputDurata" value="<?php echo $f->durata; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputNome">Ruolo Discente</label>
+        <div class="controls">
+            <input class="input-xxlarge" type="text" name="inputRapporto" id="inputRapporto" value="<?php echo $f->rapporto; ?>">
+        </div>
+    </div>
 
-<div class="control-group">
-    <div class="controls">
-      <button type="submit" class="btn btn-large btn-success">
-          <i class="icon-ok"></i>
-          Modifica Certificato
-      </button>
-  </div>
-</div>
+    <div class="control-group">
+        <div class="controls">
+            <button type="submit" class="btn btn-large btn-success">
+                <i class="icon-ok"></i>
+                Modifica Certificato
+            </button>
+        </div>
+    </div>
 
