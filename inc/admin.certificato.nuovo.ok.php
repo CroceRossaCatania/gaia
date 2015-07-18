2fa12ce0b0f18b70f7781bbeadbcb4f27edd71a1
developer <developer@gaia-vdi>
2015-07-18 04:03:52 -1000

    Certificati: Modifiche al form di inserimento e gestione dell'insert a DB

--- inc/admin.certificato.nuovo.ok.php
+++ inc/admin.certificato.nuovo.ok.php
@@ -11,6 +11,21 @@ $x = Certificato::by('nome', $_POST['inputNome']);
 if (!$x){
     $t = new Certificato();
     $t->nome = maiuscolo( $_POST['inputNome'] );
+    $t->minimoPartecipanti = intval($_POST['inputMinimoPartecipanti']);
+    $t->massimoPartecipanti = intval($_POST['inputMassimoPartecipanti']);
+    $t->durata = intval($_POST['inputDurata']);
+    $t->giorni = intval($_POST['inputGiorni']);
+    $t->limitePerIscrizione = intval($_POST['inputLimitePerIscrizione']);
+    $t->tipoValutazione = $_POST['inputTipoValutazione'];
+    $t->attestato =  $_POST['inputAttestato'];
+    $t->proporzioneAffiancamento =  intval($_POST['inputProporzioneAffiancamento']);
+    $t->punizione = intval($_POST['inputPunizione']);
+
+    $t->ruoloProprietario = $_POST['inputRuoloProprietario'];
+    $t->ruoloDirettore = $_POST['inputRuoloDirettore'];
+    $t->ruoloDocenti = $_POST['inputRuoloDocenti'];
+    $t->ruoloAffiancamento = $_POST['inputRuoloAffiancamento'];
+    $t->ruoloDiscenti = $_POST['inputRuoloDiscenti'];
     redirect('admin.certificati&new');
 } else {
     redirect('admin.certificati&dup');
--- inc/admin.certificato.nuovo.php
+++ inc/admin.certificato.nuovo.php
@@ -4,7 +4,9 @@
  * ©2013 Croce Rossa Italiana
  */
 
-paginaAdmin()
+paginaAdmin();
+
+$ruoli = array('aaa','bbb','ccc','ddd','eee');
 
 ?>
 <div class="row-fluid">
@@ -27,9 +29,135 @@ paginaAdmin()
 <form class="form-horizontal" action="?p=admin.certificato.nuovo.ok" method="POST">
     <div class="control-group">
         <label class="control-label" for="inputNome">Nome certificato</label>
-        <a href="admin.fix.comitati.php"></a>
         <div class="controls">
-            <input class="input-xxlarge" type="text" name="inputNome" id="nome" placeholder="nome del corso" required>
+            <input class="input-xxlarge" type="text" name="inputNome" id="inputNome" value="<?php echo $f->nome; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputMinimoPartecipanti">Numero discenti</label>
+        <div class="controls">
+            <span>minimo:</span>
+            <input class="input-small" type="text" name="inputMinimoPartecipanti" id="inputMinimoPartecipanti" value="<?php echo $f->minimoPartecipanti; ?>">
+            
+            <span>massimo:</span>
+            <input class="input-small" type="text" name="inputMassimoPartecipanti" id="inputMassimoPartecipanti" value="<?php echo $f->massimoPartecipanti; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputDurata">Durata</label>
+        <div class="controls">
+            <span>ore:</span>
+            <input class="input-small" type="text" name="inputDurata" id="inputDurata" value="<?php echo $f->durata; ?>">
+            
+            <span>giorni:</span>
+            <input class="input-small" type="text" name="inputGiorni" id="inputGiorni" value="<?php echo $f->giorni; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputLimitePerIscrizione">Limite in giorni per l'iscrizione</label>
+        <div class="controls">
+            <input class="input-small" type="text" name="inputLimitePerIscrizione" id="inputLimitePerIscrizione" value="<?php echo $f->limitePerIscrizione; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputAttestato">Attestato</label>
+        <div class="controls">
+            <input class="input-xxlarge" type="text" name="inputAttestato" id="inputRapporto" value="<?php echo $f->attestato; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputTipoValutazione">Tipo Valutazione</label>
+        <div class="controls">
+            <input class="input-xxlarge" type="text" name="inputTipoValutazione" id="inputTipoValutazione" value="<?php echo $f->tipoValutazione; ?>">
+        </div>
+    </div>
+    
+    <hr/>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputRuoloProprietario">Ruolo Proprietario</label>
+        <div class="controls">
+            <select class="input-xxlarge" name="inputRuoloProprietario" id="inputRuoloProprietario">
+                <option></option>
+                <?php foreach($ruoli as $r) :?>
+                    <?php $selected = ""; ?>
+                    <?php if($f->ruoloProprietario == $r):?>
+                        <?php $selected = "selected='selected'"; ?>
+                    <?php endif ?>
+                    <option <?php print $selected; ?>><?php print $r?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputRuoloDirettore">Ruolo Direttore</label>
+        <div class="controls">
+            <select class="input-xxlarge" name="inputRuoloDirettore" id="inputRuoloDirettore">
+                <option></option>
+                <?php foreach($ruoli as $r) :?>
+                    <?php $selected = ""; ?>
+                    <?php if($f->ruoloDirettore == $r):?>
+                        <?php $selected = "selected='selected'"; ?>
+                    <?php endif ?>
+                    <option <?php print $selected; ?>><?php print $r?></option>
+                <?php endforeach; ?>
+            </select> 
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputRuoloDocenti">Ruolo Docente</label>
+        <div class="controls">
+            <select class="input-xxlarge" name="inputRuoloDocenti" id="inputRuoloDocenti">
+                <option></option>
+                <?php foreach($ruoli as $r) :?>
+                    <?php $selected = ""; ?>
+                    <?php if($f->ruoloDocenti == $r):?>
+                        <?php $selected = "selected='selected'"; ?>
+                    <?php endif ?>
+                    <option <?php print $selected; ?>><?php print $r?></option>
+                <?php endforeach; ?>
+            </select> 
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputRuoloAffiancamento">Ruolo Docente di Affiancamento</label>
+        <div class="controls">
+            <select class="input-large" name="inputRuoloAffiancamento" id="inputRuoloAffiancamento">
+                <option></option>
+                <?php foreach($ruoli as $r) :?>
+                    <?php $selected = ""; ?>
+                    <?php if($f->ruoloAffiancamento == $r):?>
+                        <?php $selected = "selected='selected'"; ?>
+                    <?php endif ?>
+                    <option <?php print $selected; ?>><?php print $r?></option>
+                <?php endforeach; ?>
+            </select>
+            
+            <span>Proporzione:<span> 1 / <input class="input-small" type="text" name="inputProporzioneAffiancamento" id="inputProporzioneAffiancamento" value="<?php echo $f->proporzioneAffiancamento; ?>">
+        </div>
+    </div>
+    
+    <div class="control-group">
+        <label class="control-label" for="inputRuoloDiscenti">Ruolo Discente</label>
+        <div class="controls">
+            <select class="input-large" name="inputRuoloDiscenti" id="inputRuoloDiscenti">
+                <option></option>
+                <?php foreach($ruoli as $r) :?>
+                    <?php $selected = ""; ?>
+                    <?php if($f->ruoloDiscenti == $r):?>
+                        <?php $selected = "selected='selected'"; ?>
+                    <?php endif ?>
+                    <option <?php print $selected; ?>><?php print $r?></option>
+                <?php endforeach; ?>
+            </select>
         </div>
     </div>
     <div class="control-group">
