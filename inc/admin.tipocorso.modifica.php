0ac8cad97a8af315150daa5b25c6571687d628f6
Pietro Ferraresi <pietro.ferraresi@gmail.com>
2015-10-17 16:49:02 +0200

    Aggiunti i ruoli e modificata la creazione del tipo di corso

--- inc/admin.tipocorso.modifica.php
+++ inc/admin.tipocorso.modifica.php
@@ -8,7 +8,7 @@ controllaParametri(array('id'), 'admin.tipocorso&err');
 $t = $_GET['id'];
 $f = TipoCorso::id($t);
 
-$ruoli = array('aaa','bbb','ccc','ddd','eee');
+$ruoli = RuoloFormazione::elencoRuoli();
 ?>
 
 <style>
@@ -38,6 +38,22 @@ $ruoli = array('aaa','bbb','ccc','ddd','eee');
     </div>
     
     <div class="control-group">
+        <label class="control-label" for="inputRuoloAttestato">Ruolo Attestato</label>
+        <div class="controls">
+            <select class="input-xxlarge" name="inputRuoloAttestato" id="inputRuoloAttestato">
+                <option></option>
+                <?php foreach($ruoli as $r) :?>
+                    <?php $selected = ""; ?>
+                    <?php if(intval($f->ruoloAttestato) === intval($r->id)):?>
+                        <?php $selected = "selected='selected'"; ?>
+                    <?php endif ?>
+                    <option <?php print $selected; ?>><?php print $r->ruolo?></option>
+                <?php endforeach; ?>
+            </select> 
+        </div>
+    </div>
+    
+    <div class="control-group">
         <label class="control-label" for="inputMinimoPartecipanti">Numero discenti</label>
         <div class="controls">
             <span>minimo:</span>
@@ -57,6 +73,19 @@ $ruoli = array('aaa','bbb','ccc','ddd','eee');
             <span>giorni:</span>
             <input class="input-small" type="text" name="inputGiorni" id="inputGiorni" value="<?php echo $f->giorni; ?>">
         </div>
+        
+        <?php /*
+        $abilitaLezioni = "";
+        if ($f->abilitaLezioni){
+            $abilitaLezioni = 'checked="checked"';
+        }
+        ?>
+        <label class="control-label" for="abilitaLezioni">Abilita la gestione delle lezioni</label>
+        <div class="controls">
+            <input type="checkbox" name="abilitaLezioni" id="abilitaLezioni" value="1" <?php print $abilitaLezioni ?> />
+        </div>
+         * 
+         */ ?>
     </div>
     
     <div class="control-group">
@@ -81,23 +110,7 @@ $ruoli = array('aaa','bbb','ccc','ddd','eee');
     </div>
     
     <hr/>
-    
-    <div class="control-group">
-        <label class="control-label" for="inputRuoloProprietario">Ruolo Proprietario</label>
-        <div class="controls">
-            <select class="input-xxlarge" name="inputRuoloProprietario" id="inputRuoloProprietario">
-                <option></option>
-                <?php foreach($ruoli as $r) :?>
-                    <?php $selected = ""; ?>
-                    <?php if($f->ruoloProprietario == $r):?>
-                        <?php $selected = "selected='selected'"; ?>
-                    <?php endif ?>
-                    <option <?php print $selected; ?>><?php print $r?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-    </div>
-    
+
     <div class="control-group">
         <label class="control-label" for="inputRuoloDirettore">Ruolo Direttore</label>
         <div class="controls">
@@ -105,10 +118,10 @@ $ruoli = array('aaa','bbb','ccc','ddd','eee');
                 <option></option>
                 <?php foreach($ruoli as $r) :?>
                     <?php $selected = ""; ?>
-                    <?php if($f->ruoloDirettore == $r):?>
+                    <?php if(intval($f->ruoloDirettore) === intval($r->id)):?>
                         <?php $selected = "selected='selected'"; ?>
                     <?php endif ?>
-                    <option <?php print $selected; ?>><?php print $r?></option>
+                     <option <?php print $selected; ?> value="<?php print $r->id?> "><?php print $r->ruolo?></option>
                 <?php endforeach; ?>
             </select> 
         </div>
@@ -121,10 +134,10 @@ $ruoli = array('aaa','bbb','ccc','ddd','eee');
                 <option></option>
                 <?php foreach($ruoli as $r) :?>
                     <?php $selected = ""; ?>
-                    <?php if($f->ruoloDocenti == $r):?>
+                    <?php if(intval($f->ruoloDocenti) === intval($r->id)):?>
                         <?php $selected = "selected='selected'"; ?>
                     <?php endif ?>
-                    <option <?php print $selected; ?>><?php print $r?></option>
+                     <option <?php print $selected; ?> value="<?php print $r->id?> "><?php print $r->ruolo?></option>
                 <?php endforeach; ?>
             </select> 
         </div>
@@ -137,10 +150,10 @@ $ruoli = array('aaa','bbb','ccc','ddd','eee');
                 <option></option>
                 <?php foreach($ruoli as $r) :?>
                     <?php $selected = ""; ?>
-                    <?php if($f->ruoloAffiancamento == $r):?>
+                    <?php if(intval($f->ruoloAffiancamento) === intval($r->id)):?>
                         <?php $selected = "selected='selected'"; ?>
                     <?php endif ?>
-                    <option <?php print $selected; ?>><?php print $r?></option>
+                     <option <?php print $selected; ?> value="<?php print $r->id?> "><?php print $r->ruolo?></option>
                 <?php endforeach; ?>
             </select>
             
@@ -156,10 +169,10 @@ $ruoli = array('aaa','bbb','ccc','ddd','eee');
                 <option></option>
                 <?php foreach($ruoli as $r) :?>
                     <?php $selected = ""; ?>
-                    <?php if($f->ruoloDiscenti == $r):?>
+                    <?php if(intval($f->ruoloDiscenti) === intval($r->id)):?>
                         <?php $selected = "selected='selected'"; ?>
                     <?php endif ?>
-                    <option <?php print $selected; ?>><?php print $r?></option>
+                    <option <?php print $selected; ?> value="<?php print $r->id?> "><?php print $r->ruolo?></option>
                 <?php endforeach; ?>
             </select>
             
