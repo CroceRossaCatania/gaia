2e4eeb212dbcabe96e71b28ef5213343bf97fdd3
rad <marco.radossi@gmail.com>
2015-11-01 15:23:22 +0100

    fix aggiunta civile

--- assets/js/formazione.corsi.discenti.js
+++ assets/js/formazione.corsi.discenti.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
 
-    $('#nuova-persona').on('submit', function () {
+    $('##nuova-persona').on('submit', function () {
 
         api('aggiungi:civile', {
-                nome: $('inputNome').val(),
-                cognome: $('inputCognome').val(),
-                sesso: $('inputSesso').val(),
-                dnascita: $('inputDataNascita').val(),
-                prnascita: $('inputProvinciaNascita').val(),
-                conascita: $('inputComuneNascita').val(),
-                coresidenza: $('inputComuneResidenza').val(),
-                caresidenza: $('inputCAPResidenza').val(),
-                prresidenza: $('inputProvinciaResidenza').val(),
-                indirizzo: $('inputIndirizzo').val(),
-                civico: $('inputCivico').val()
+                "nome": $('#inputNome').val(),
+                "cognome": $('#inputCognome').val(),
+                "sesso": $('#inputSesso').val(),
+                "dnascita": $('#inputDataNascita').val(),
+                "prnascita": $('#inputProvinciaNascita').val(),
+                "conascita": $('#inputComuneNascita').val(),
+                "coresidenza": $('#inputComuneResidenza').val(),
+                "caresidenza": $('#inputCAPResidenza').val(),
+                "prresidenza": $('#inputProvinciaResidenza').val(),
+                "indirizzo": $('#inputIndirizzo').val(),
+                "civico": $('#inputCivico').val()
             }, 
             function (x) {
                 console.log(x);
