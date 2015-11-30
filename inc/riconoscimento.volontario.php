a8d3f47b481e85f479604411d87d630f1571abab
Federico <ico88@hotmail.com>
2015-11-30 22:22:23 +0100

    Modifiche grammatiche

--- inc/riconoscimento.volontario.php
+++ inc/riconoscimento.volontario.php
@@ -10,15 +10,14 @@
   <div class="span4">
     <h2>
       <i class="icon-question-sign muted"></i>
-      Volontario, non riesci a registrarti?
+      Sei Volontario CRI e non sei ancora iscritto su GAIA?
     </h2>
   </div>
   <div class="span8">
     <p>
       Ciao,
-      dal 1° dicembre non è più possibile registrarsi come volontario autonomamente sul gestionale GAIA.
-      Tale funzionalità, pensata inizialmente per favorire l'autonoma iscrizione dei soci sul gestionale, operazione ormai completata nella maggior parte dei Comitati, oggi consentirebbe da una parte alterazioni degli elenchi soci nel corso dell'anno e dall'altra confusione degli aspiranti che intendono iscriversi ai corsi sul gestionale.
-      Per ogni necessità puoi rivolgerti in primo luogo ai vostri GAIA Supporter (che hanno un accesso prioritario al supporto) e, solo dopo il confronto con loro, al supporto tecnico.
-    </p>
+dal 1° dicembre 2015 per registrarsi a GAIA come Volontario CRI occorre richiederlo al proprio ufficio soci facendo compiere l'operazione a loro dagli appositi pannelli. Mentre una volta era possibile registrarsi in autonomia, oggi è necessario che l'inserimento avvenga a cura del personale preposto del tuo Comitato. Ciò assicura da una parte che i libri soci non subiscano alterazioni, dall'altra semplifica la navigazione su GAIA agli aspiranti Volontari.
+Per ogni necessità puoi rivolgerti in primo luogo al tuo ufficio soci e in caso di necessità al supporto tecnico.
+</p>
   </div>
 </div>
