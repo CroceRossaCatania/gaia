f3ff4648b0a48de6122696144bb29749f8fb8f65
Federico <ico88@hotmail.com>
2015-11-30 22:02:21 +0100

    Inserimento di una pagina di chiarimento

---
+++ inc/riconoscimento.volontario.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * ©2015 Croce Rossa Italiana
+ */
+
+?>
+
+<div class="row-fluid">
+  <div class="span4">
+    <h2>
+      <i class="icon-question-sign muted"></i>
+      Volontario, non riesci a registrarti?
+    </h2>
+  </div>
+  <div class="span8">
+    <p>
+      Ciao,
+      dal 1° dicembre non è più possibile registrarsi come volontario autonomamente sul gestionale GAIA.
+      Tale funzionalità, pensata inizialmente per favorire l'autonoma iscrizione dei soci sul gestionale, operazione ormai completata nella maggior parte dei Comitati, oggi consentirebbe da una parte alterazioni degli elenchi soci nel corso dell'anno e dall'altra confusione degli aspiranti che intendono iscriversi ai corsi sul gestionale.
+      Per ogni necessità puoi rivolgerti in primo luogo ai vostri GAIA Supporter (che hanno un accesso prioritario al supporto) e, solo dopo il confronto con loro, al supporto tecnico.
+    </p>
+  </div>
+</div>
