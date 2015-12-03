92a49272223a7d9fd019c211da8ad19e9fc9c347
Alfio Emanuele <alfio.emanuele.f@gmail.com>
2015-12-03 13:03:50 +0000

    Notifica '2' in Menu Formazione

--- index.php
+++ index.php
@@ -126,7 +126,7 @@ $_descrizione   = 'Crediamo in una Croce Rossa Italiana che sa muoversi veloceme
                             <li><a href="index.php"><i class="icon-home"></i> Home</a></li>
                             <li><a href="?p=attivita"><i class="icon-calendar"></i> Attività</a></li>
                             <li><a href="?p=public.comitati.mappa"><i class="icon-map-marker"></i> Comitati</a></li>
-                            <li><a href="?p=public.formazione"><i class="icon-desktop"></i> Formazione</a></li>
+                            <li><a href="?p=public.formazione"><i class="icon-desktop"></i> Formazione <span class="badge badge-success">2</span></a></li>
 							<?php if(!$me) { ?>
                             <li><a href="?p=public.tesserino"><i class="icon-credit-card"></i> Verifica tesserino</a></li>
                             <?php } ?>
